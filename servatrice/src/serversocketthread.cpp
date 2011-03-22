3f5035c38c1004614b872ef0ef05346197a23f0f
Max-Wilhelm Bruker <brukie@gmx.net>
2011-03-22 18:03:26 +0100

    added missing files

---
+++ servatrice/src/serversocketthread.cpp
@@ -0,0 +1,29 @@
+#include "serversocketthread.h"
+#include "serversocketinterface.h"
+#include "server_logger.h"
+#include "main.h"
+
+ServerSocketThread::ServerSocketThread(int _socketDescriptor, Servatrice *_server, QObject *parent)
+	: QThread(parent), server(_server), socketDescriptor(_socketDescriptor)
+{
+}
+
+ServerSocketThread::~ServerSocketThread()
+{
+	quit();
+	wait();
+}
+
+void ServerSocketThread::run()
+{
+	QTcpSocket *socket = new QTcpSocket;
+	socket->setSocketDescriptor(socketDescriptor);
+	logger->logMessage(QString("incoming connection: %1").arg(socket->peerAddress().toString()));
+	
+	ssi = new ServerSocketInterface(server, socket);
+	connect(ssi, SIGNAL(destroyed()), this, SLOT(deleteLater()));
+	
+	emit clientAdded(ssi);
+	
+	exec();
+}
---
+++ servatrice/src/serversocketthread.h
@@ -0,0 +1,24 @@
+#ifndef SERVERSOCKETTHREAD_H
+#define SERVERSOCKETTHREAD_H
+
+#include <QThread>
+
+class Servatrice;
+class ServerSocketInterface;
+
+class ServerSocketThread : public QThread {
+	Q_OBJECT
+private:
+	Servatrice *server;
+	ServerSocketInterface *ssi;
+	int socketDescriptor;
+public:
+	ServerSocketThread(int _socketDescriptor, Servatrice *_server, QObject *parent = 0);
+	~ServerSocketThread();
+protected:
+	void run();
+signals:
+	void clientAdded(ServerSocketInterface *client);
+};
+
+#endif
