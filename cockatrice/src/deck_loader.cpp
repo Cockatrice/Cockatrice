Cockatrice/Cockatrice

commit 67c4d089be15f2704bcdb76f4ed3dfceddafdc73

Deck Load Mods and Movable Tabs
Changes:
Deck Load - Default to all types *.*
Decklist - Strip "|edition" from .dec formats that include a pipe and
the edition after the card name
Tab_Supervisor - Make tabs movable (able to rearrange tabs)

cockatrice/src/deck_loader.cpp
@@ -4,9 +4,9 @@
 #include "decklist.h"
 
 const QStringList DeckLoader::fileNameFilters = QStringList()
+	<< QObject::tr("All files (*.*)")
 	<< QObject::tr("Cockatrice decks (*.cod)")
-	<< QObject::tr("Plain text decks (*.dec *.mwDeck)")
-	<< QObject::tr("All files (*.*)");
+	<< QObject::tr("Plain text decks (*.dec *.mwDeck)");
 
 DeckLoader::DeckLoader()
 	: DeckList(),
@@ -94,8 +94,8 @@ bool DeckLoader::saveToFile(const QString &fileName, FileFormat fmt)
 DeckLoader::FileFormat DeckLoader::getFormatFromNameFilter(const QString &selectedNameFilter)
 {
 	switch (fileNameFilters.indexOf(selectedNameFilter)) {
-		case 0: return CockatriceFormat;
-		case 1: return PlainTextFormat;
+		case 1: return CockatriceFormat;
+		case 2: return PlainTextFormat;
 	}
 	return PlainTextFormat;
 }

cockatrice/src/tab_supervisor.cpp
@@ -80,6 +80,7 @@ TabSupervisor::TabSupervisor(AbstractClient *_client, QWidget *parent)
 {
 	tabChangedIcon = new QIcon(":/resources/icon_tab_changed.svg");
 	setElideMode(Qt::ElideRight);
+	setMovable(true);
 	setIconSize(QSize(15, 15));
 	connect(this, SIGNAL(currentChanged(int)), this, SLOT(updateCurrent(int)));
 

common/decklist.cpp
@@ -449,7 +449,11 @@ bool DeckList::loadFromStream_Plain(QTextStream &in)
 		line.remove(rx);
 		rx.setPattern("\\(.*\\)");
 		line.remove(rx);
+		//Filter out post card name editions		
+		rx.setPattern("\\|.*");
+		line.remove(rx);
 		line = line.simplified();
+
 
 		int i = line.indexOf(' ');
 		bool ok;

commit 6ecb3e30c1c7b57fcebc832de7a8391a266a4611

Fixes #34
This issue is actually present in my stable version of Cockatrice. The
problem is that Cockatrice identifies the file type by the file type
dialog, which is one of "All files", "Cockatrice decks", or "Plain text
decks". However, it can't figure out what to do with "All files", since
by definition that oughtn't have a structure associated with it. When
using "all files" as the deck type, Cockatrice fails in the listed
manner.

Nothing broke in commit 67c4d08, but "all files" was moved to the top
(presumably cosmetically), so now people are hitting it accidentally.
To fix this, I just removed "all files". This is the right thing to do
because 1) people didn't seem to be having this issue before,
indicating nobody used "all files", so removing it won't affect
anybody, and 2) Cockatrice can't know what to do with the file anyway,
so there's no simple way to keep it.

cockatrice/src/deck_loader.cpp
@@ -4,7 +4,6 @@
 #include "decklist.h"
 
 const QStringList DeckLoader::fileNameFilters = QStringList()
-	<< QObject::tr("All files (*.*)")
 	<< QObject::tr("Cockatrice decks (*.cod)")
 	<< QObject::tr("Plain text decks (*.dec *.mwDeck)");
 
@@ -94,8 +93,8 @@ bool DeckLoader::saveToFile(const QString &fileName, FileFormat fmt)
 DeckLoader::FileFormat DeckLoader::getFormatFromNameFilter(const QString &selectedNameFilter)
 {
 	switch (fileNameFilters.indexOf(selectedNameFilter)) {
-		case 1: return CockatriceFormat;
-		case 2: return PlainTextFormat;
+		case 0: return CockatriceFormat;
+		case 1: return PlainTextFormat;
 	}
 	return PlainTextFormat;
 }