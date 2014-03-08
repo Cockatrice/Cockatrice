a15eb6f29fcfba8657ee2d782afb1e72343a4610
Mark Morschhäuser <mark@tvk.rwth-aachen.de>
2014-03-08 17:08:40 +0100

    Updated SFMT RNG code, removed Qt RNG

--- common/rng_sfmt.cpp
+++ common/rng_sfmt.cpp
@@ -1,5 +1,4 @@
 #include "rng_sfmt.h"
-#include "sfmt/SFMT.h"
 #include <QDateTime>
 #include <stdlib.h>
 #include <iostream>
@@ -7,19 +6,17 @@
 RNG_SFMT::RNG_SFMT(QObject *parent)
 	: RNG_Abstract(parent)
 {
-	std::cerr << "Using SFMT random number generator." << std::endl;
-	
-	int seed = QDateTime::currentDateTime().toTime_t();
-	init_gen_rand(seed);
-	for (int i = 0; i < 100000; ++i)
-		gen_rand64();
+	// initialize the random number generator with a 32bit integer seed (timestamp)
+	sfmt_init_gen_rand(&sfmt, QDateTime::currentDateTime().toTime_t());
 }
 
 unsigned int RNG_SFMT::getNumber(unsigned int min, unsigned int max)
 {
+	// To make the random number generation thread safe, a mutex is created around the generation.
 	mutex.lock();
-	uint64_t r = gen_rand64();
+	uint64_t r = sfmt_genrand_uint64(&sfmt);
 	mutex.unlock();
 	
-	return min + (unsigned int) (((double) (max + 1 - min)) * r / (18446744073709551616.0 + 1.0));
+	// return a random number from the interval [min, max]
+	return (unsigned int) (r % (max - min + 1));
 }
--- common/rng_sfmt.h
+++ common/rng_sfmt.h
@@ -1,13 +1,27 @@
 #ifndef RNG_SFMT_H
 #define RNG_SFMT_H
 
+#include "sfmt/SFMT.h"
 #include "rng_abstract.h"
 #include <QMutex>
+
+/**
+ * This class represents the random number generator.
+ * It uses the SIMD-oriented Fast Mersenne Twister code v1.4.1 from
+ * http://www.math.sci.hiroshima-u.ac.jp/~%20m-mat/MT/SFMT/index.html
+ * To use this RNG, the class needs a sfmt_t structure for the RNG's internal state.
+ * It has to be initialized by sfmt_init_gen_rand() which is done in the constructor.
+ * The function sfmt_genrand_uint64() can then be used to create a 64 bit unsigned int 
+ * pseudorandom number. This is done in getNumber().
+ * For more information see the author's website and look at the documentation and 
+ * examples that are part of the official downloads.
+ */
 
 class RNG_SFMT : public RNG_Abstract {
 	Q_OBJECT
 private:
 	QMutex mutex;
+	sfmt_t sfmt;
 public:
 	RNG_SFMT(QObject *parent = 0);
 	unsigned int getNumber(unsigned int min, unsigned int max);
