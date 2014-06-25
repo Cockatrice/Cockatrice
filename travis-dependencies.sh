4e4401a9e2e2d6366e0c3fb8c1da4abf4034a053
Fabio Bas <ctrlaltca@gmail.com>
2014-06-25 18:39:26 +0200

    maybe fix travis build for ubuntu

--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -5,5 +5,5 @@ if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
   brew install qt cmake protobuf
 else
   sudo apt-get update -qq
-  sudo apt-get install -y qtmobility-dev libprotobuf-dev protobuf-compiler libqt4-dev
+  sudo apt-get install -y qtmobility-dev libprotobuf-dev protobuf-compiler libqt4-dev qt4-dev-tools
 fi
