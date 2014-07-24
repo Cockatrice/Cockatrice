13bd35e020ef2508ab442a3fb5f02e875b76cdac
Fabio Bas <ctrlaltca@gmail.com>
2014-07-24 18:16:08 +0200

    Servatrice: add prototypes for local functions

--- servatrice/src/main.cpp
+++ servatrice/src/main.cpp
@@ -37,6 +37,23 @@ RNG_Abstract *rng;
 ServerLogger *logger;
 QThread *loggerThread;
 
+/* Prototypes */
+
+void testRNG();
+void testHash();
+#if QT_VERSION < 0x050000
+void myMessageOutput(QtMsgType type, const char *msg);
+void myMessageOutput2(QtMsgType type, const char *msg);
+#else
+void myMessageOutput(QtMsgType type, const QMessageLogContext &, const QString &msg);
+void myMessageOutput2(QtMsgType type, const QMessageLogContext &, const QString &msg);
+#endif
+#ifdef Q_OS_UNIX
+void sigSegvHandler(int sig);
+#endif
+
+/* Implementations */
+
 void testRNG()
 {
 	const int n = 500000;
