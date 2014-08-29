ab83d6185a6b6e4017fc659e3b4636c3cd7910a6
Antony Woods <acron1@gmail.com>
2014-08-29 16:53:30 +0100

    Used tr() function for auto-connect label. Changed formatting to adhere to coding style guidelines. 'Save Password' setting is now saved when cancelling the Connect dialog

--- cockatrice/src/settingscache.cpp
+++ cockatrice/src/settingscache.cpp
@@ -49,7 +49,7 @@ SettingsCache::SettingsCache()
 
     ignoreUnregisteredUsers = settings->value("chat/ignore_unregistered", false).toBool();
 
-    attemptAutoConnect = settings->value("server/auto_connect", 0).toInt() == 0 ? false : true; 
+    attemptAutoConnect = settings->value("server/auto_connect", 0).toBool(); 
 }
 
 void SettingsCache::setLang(const QString &_lang)
