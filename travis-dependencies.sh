a91106f90ec36deb3bd1e27cf833849a5024fa91
Gavin Bisesi <Daenyth+github@gmail.com>
2014-11-10 15:36:31 -0500

    Add ppa for qt5 on travis ubuntu

--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -4,6 +4,8 @@ if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
   brew update
   brew install qt qt5 protobuf libgcrypt
 else
+  sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
   sudo apt-get update -qq
-  sudo apt-get install -y qtmobility-dev libprotobuf-dev protobuf-compiler libqt4-dev qt5-default qttools5-dev-tools
+  sudo apt-get install -y qtmobility-dev libprotobuf-dev protobuf-compiler libqt4-dev\
+   qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qt5-default qttools5-dev-tools
 fi
