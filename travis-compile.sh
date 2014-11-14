5f944f1d6ea57a324fd67a9e554d4e0dd8c6b340
Gavin Bisesi <Daenyth+github@gmail.com>
2014-11-14 16:32:56 -0500

    Actually use Qt5 for qt5-osx build

---
+++ travis-compile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+mkdir build
+cd build
+prefix=""
+if [[ $TRAVIS_OS_NAME == "osx" && $QT4 == 0 ]]; then
+  prefix="-DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt5/5.3.0/"
+fi
+cmake .. -DWITH_SERVER=1 -DWITH_QT4=$QT4 $prefix
+make
