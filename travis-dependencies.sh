e918900d4e66020dd9e5fe87b53c5b3755dc73e2
Gavin Bisesi <Daenyth+github@gmail.com>
2014-11-14 10:38:54 -0500

    Refine travis config to only install qt4 or qt5 per build instead of both

--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -2,10 +2,19 @@
 
 if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
   brew update
-  brew install qt qt5 protobuf libgcrypt
+  if (( QT4 )); then
+    brew install qt protobuf libgcrypt
+  else
+    brew install qt qt5 protobuf libgcrypt
+  fi
 else
-  sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
-  sudo apt-get update -qq
-  sudo apt-get install -y qtmobility-dev libprotobuf-dev protobuf-compiler libqt4-dev\
-   qtbase5-dev qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qt5-default qttools5-dev-tools
+  if (( QT4 )); then
+    sudo apt-get update -qq
+    sudo apt-get install -y qtmobility-dev libprotobuf-dev protobuf-compiler libqt4-dev
+  else
+    sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+    sudo apt-get update -qq
+    sudo apt-get install -y libprotobuf-dev protobuf-compiler qtbase5-dev\
+      qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qt5-default qttools5-dev-tools
+  fi
 fi
