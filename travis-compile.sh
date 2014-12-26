49310b529187e5ae997e243d8f0ef96828a4e322
Fabio Bas <ctrlaltca@gmail.com>
2014-12-26 16:34:42 +0100

    Fix travis compilation with osx+qt5
    Homebrew currently installs qt 5.4.0

--- travis-compile.sh
+++ travis-compile.sh
@@ -6,7 +6,7 @@ mkdir build
 cd build
 prefix=""
 if [[ $TRAVIS_OS_NAME == "osx" && $QT4 == 0 ]]; then
-  prefix="-DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt5/5.3.2/"
+  prefix="-DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt5/5.4.0/"
 fi
 cmake .. -DWITH_SERVER=1 -DWITH_QT4=$QT4 $prefix
 make
