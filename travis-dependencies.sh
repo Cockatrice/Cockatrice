4d9da5232b51663861ec3074de61a9ef8be70130
Fabio Bas <ctrlaltca@gmail.com>
2014-12-30 23:32:19 +0100

    happy new mess

--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -17,7 +17,7 @@ else
     sudo apt-get install -y libprotobuf-dev protobuf-compiler qtbase5-dev cmake\
       qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qt5-default qttools5-dev-tools\
       qttools5-dev libqt5svg5-dev libqt5svg5 qtbase5-dev libqt5gui5 libqt5printsupport5 \
-      libqt5widgets5 qt5-qmake qtbase5-dev-tools qtchooser libqt5quick5 libqt5quickparticles5 \
+      libqt5widgets5 qt5-qmake qtbase5-dev-tools libqt5quick5 libqt5quickparticles5 \
       libqt5quicktest5 libqt5designer5 libqt5designercomponents5 libqt5gui5 libqt5help5
   fi
 fi
