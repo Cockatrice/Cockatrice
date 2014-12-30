977fb4c99c8c166b296d30addb1826aa52608b3b
Fabio Bas <ctrlaltca@gmail.com>
2014-12-30 23:27:57 +0100

    adding libqt5-whatever-but-not-this-dependency-hell

--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -16,6 +16,8 @@ else
     sudo apt-get update -qq
     sudo apt-get install -y libprotobuf-dev protobuf-compiler qtbase5-dev cmake\
       qtdeclarative5-dev libqt5webkit5-dev libsqlite3-dev qt5-default qttools5-dev-tools\
-      qttools5-dev libqt5svg5-dev
+      qttools5-dev libqt5svg5-dev libqt5svg5 qtbase5-dev libqt5gui5 libqt5printsupport5 \
+      libqt5widgets5 qt5-qmake qtbase5-dev-tools qtchooser libqt5quick5 libqt5quickparticles5 \
+      libqt5quicktest5 libqt5designer5 libqt5designercomponents5 libqt5gui5 libqt5help5
   fi
 fi
