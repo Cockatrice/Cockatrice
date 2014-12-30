fe1ad8f27ce296749a3f4af784521d24f600b3e3
Michael Callahan <coder.callahan@gmail.com>
2014-12-29 23:45:34 -0700

    Fix unused and reorder warnings.

--- oracle/src/oracleimporter.h
+++ oracle/src/oracleimporter.h
@@ -21,7 +21,7 @@ public:
     bool getImport() const { return import; }
     void setImport(bool _import) { import = _import; }
     SetToDownload(const QString &_shortName, const QString &_longName, const QVariant &_cards, bool _import, const QString &_setType = QString(), const QDate &_releaseDate = QDate())
-        : shortName(_shortName), longName(_longName), import(_import), cards(_cards), setType(_setType), releaseDate(_releaseDate)  { }
+        : shortName(_shortName), longName(_longName), import(_import), cards(_cards), releaseDate(_releaseDate), setType(_setType)  { }
     bool operator<(const SetToDownload &set) const { return longName.compare(set.longName, Qt::CaseInsensitive) < 0; }
 };
 
