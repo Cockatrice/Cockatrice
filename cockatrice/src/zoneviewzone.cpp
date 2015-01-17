54a1a28619f05b071af2de8f1bc9168b132c922b
Matt Lowe <matthewneillowe@gmail.com>
2015-01-18 00:18:23 +0100

    Added ratio scaling
    Previously was adding pixels, rather than using ratios. this was cutting
    off cards on higher res screens

--- cockatrice/src/zoneviewzone.cpp
+++ cockatrice/src/zoneviewzone.cpp
@@ -120,8 +120,8 @@ void ZoneViewZone::reorganizeCards()
     
     qreal aleft = 0;
     qreal atop = 0;
-    qreal awidth = (pileView && sortByType) ? qMax(typeColumn + 1, 3) * CARD_WIDTH + 20 : qMax(cols, 1) * CARD_WIDTH + 20;
-    qreal aheight = (pileView && sortByType) ? ((longestRow - 1) * CARD_HEIGHT) / 3 + CARD_HEIGHT + 60 : ((rows - 1) * CARD_HEIGHT) / 3 + CARD_HEIGHT + 20;
+    qreal awidth = (pileView && sortByType) ? qMax(typeColumn + 1, 3) * CARD_WIDTH + (CARD_WIDTH/2) : qMax(cols, 1) * CARD_WIDTH + (CARD_WIDTH/2);
+    qreal aheight = (pileView && sortByType) ? (longestRow * CARD_HEIGHT) / 3 + CARD_HEIGHT * 1.3 : (rows * CARD_HEIGHT) / 3 + CARD_HEIGHT * 1.3;
     optimumRect = QRectF(aleft, atop, awidth, aheight);
 
     updateGeometry();
