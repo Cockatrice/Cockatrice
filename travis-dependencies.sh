11d77f8324fdf8af4bf8eb76d20fdc31d95846ac
Fabio Bas <ctrlaltca@gmail.com>
2015-02-19 14:52:09 +0100

    Even make is too old.. 2.8.9 is required, this repo offers 2.8.11

--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -12,7 +12,8 @@ else
     sudo apt-get update -qq
     sudo apt-get install -y qtmobility-dev libprotobuf-dev protobuf-compiler libqt4-dev
   else
-    sudo add-apt-repository --yes ppa:beineri/opt-qt521
+    sudo add-apt-repository -y ppa:beineri/opt-qt521
+    sudo add-apt-repository -y ppa:kalakris/cmake
     sudo apt-get update -qq
     sudo apt-get install -y libprotobuf-dev protobuf-compiler cmake libsqlite3-dev\
       qt52base qt52webkit qt52tools qt52svg
