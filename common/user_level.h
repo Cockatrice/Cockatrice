4d8218da13d609d7e3202828d2df383fa71da68f
Gavin Bises <daenyth@gmail.com>
2015-02-21 07:16:51 -0500

    Avoid compilation failure due to protobuf bug on osx
    https://code.google.com/p/protobuf/issues/detail?id=119

--- common/user_level.h
+++ common/user_level.h
@@ -1,6 +1,11 @@
 #ifndef USER_LEVEL_H
 #define USER_LEVEL_H
 
+#ifdef Q_OS_OSX
+// avoid collision from Mac OS X's ConditionalMacros.h
+// https://code.google.com/p/protobuf/issues/detail?id=119
+#undef TYPE_BOOL
+#endif
 #include "pb/serverinfo_user.pb.h"
 #include <QFlags>
 
