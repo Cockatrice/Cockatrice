5b6624280a924786932e6bc73d1af856a5dbb10c
Gavin Bises <daenyth@gmail.com>
2015-03-18 22:12:30 -0400

    Rearrange register script

--- servatrice/scripts/register.py
+++ servatrice/scripts/register.py
@@ -3,12 +3,26 @@
 from pypb.session_commands_pb2 import Command_Register as Reg
 from pypb.commands_pb2 import CommandContainer as Cmd
 
-if __name__ == "__main__":
-    print "Attempting to register"
-    reg = Reg()
+CMD_ID = 1
+
+class Callback:
+    def run(self, response):
+        print "Got response: %s" % response
+
+def build_reg():
+    cmd = Cmd()
+    sc = cmd.session_command.add()
+
+    reg = sc.Extensions[Reg.ext]
     reg.user_name = "testUser"
     reg.email = "test@example.com"
     reg.password = "password"
-    cmd = Cmd()
-    sc = cmd.session_command.add()
-    sc.MESSAGE = r.EXT_FIELD_NUMBER
+
+    cmd.cmd_id = CMD_ID
+    CMD_ID += 1
+    return cmd
+
+if __name__ == "__main__":
+    print "Building registration command"
+    r = build_reg()
+    print "Attempting to register"
