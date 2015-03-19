a2a11c4bde998a21f7d837dcca71a184ddf11344
Gavin Bises <daenyth@gmail.com>
2015-03-18 21:22:57 -0400

    Stub register script initial attempt

--- servatrice/scripts/register.py
+++ servatrice/scripts/register.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python
+
+from pypb.session_commands_pb2 import Command_Register as Reg
+from pypb.commands_pb2 import CommandContainer as Cmd
+
+if __name__ == "__main__":
+    print "Attempting to register"
+    reg = Reg()
+    reg.user_name = "testUser"
+    reg.email = "test@example.com"
+    reg.password = "password"
+    cmd = Cmd()
+    sc = cmd.session_command.add()
+    sc.MESSAGE = r.EXT_FIELD_NUMBER
