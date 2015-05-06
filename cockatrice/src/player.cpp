5dfef6215c2e8975ce146d3578da369668ef2e11
Matt Lowe <matthewneillowe@gmail.com>
2015-05-06 14:19:01 +0200

    Added punctuation mark

--- cockatrice/src/player.cpp
+++ cockatrice/src/player.cpp
@@ -523,7 +523,7 @@ void Player::playerListActionTriggered()
 #else
             QInputDialog::getInt(
 #endif
-            0, tr("Reveal top cards of library"), tr("Number of cards: (max %1)").arg(decksize), defaultNumberTopCards, 1, decksize, 1, &ok);
+            0, tr("Reveal top cards of library"), tr("Number of cards: (max. %1)").arg(decksize), defaultNumberTopCards, 1, decksize, 1, &ok);
         if (ok) {
             cmd.set_zone_name("deck");
             cmd.set_top_cards(number);
