8cf44616161e4e5e5ec2d055fd175733012d5b63
Fabio Bas <ctrlaltca@gmail.com>
2015-05-24 01:00:55 +0200

    fix qt4 compilation

--- servatrice/src/passwordhasher.cpp
+++ servatrice/src/passwordhasher.cpp
@@ -4,10 +4,9 @@
     #include <stdio.h>
     #include <string.h>
     #include <gcrypt.h>
-#else
-    #include <QCryptographicHash>
 #endif
 
+#include <QCryptographicHash>
 #include "rng_sfmt.h"
 
 void PasswordHasher::initialize()
