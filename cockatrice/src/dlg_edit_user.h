c9e15853228e357c195b4a69ace31d3a0d63d08b
Fabio Bas <ctrlaltca@gmail.com>
2015-07-16 17:59:28 +0200

    Fix change flag in edit account; Fix #1269

--- cockatrice/src/dlg_edit_user.h
+++ cockatrice/src/dlg_edit_user.h
@@ -15,7 +15,7 @@ public:
     DlgEditUser(QWidget *parent = 0, QString email = QString(), int gender = -1, QString country = QString(), QString realName = QString());
     QString getEmail() const { return emailEdit->text(); }
     int getGender() const { return genderEdit->currentIndex() - 1; }
-    QString getCountry() const { return genderEdit->currentIndex() == 0 ? "" : countryEdit->currentText(); }
+    QString getCountry() const { return countryEdit->currentIndex() == 0 ? "" : countryEdit->currentText(); }
     QString getRealName() const { return realnameEdit->text(); }
 private slots:
     void actOk();
