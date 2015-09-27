7d7151135ebf1dd56c420c108d6764d4fdf93dd9
woogerboy21 <woogerboy21@gmail.com>
2015-09-26 23:19:49 -0400

    Removed mod notification code that causes crashes

--- common/server.h
+++ common/server.h
@@ -57,7 +57,6 @@ public:
     virtual QMap<QString, bool> getServerRequiredFeatureList() const { return QMap<QString, bool>(); }
     void addClient(Server_ProtocolHandler *player);
     void removeClient(Server_ProtocolHandler *player);
-    QList<QString> getOnlineModeratorList();
     virtual QString getLoginMessage() const { return QString(); }
     virtual bool permitUnregisteredUsers() const { return true; }
     virtual bool getGameShouldPing() const { return false; }
