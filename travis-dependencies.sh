8001b05ce750d7a936c60cc9583a6a1fc00a91a9
Fabio Bas <ctrlaltca@gmail.com>
2015-10-06 08:38:09 +0200

    Travis - deploy to bintray

--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -9,23 +9,41 @@ if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
   fi
   brew unlink cmake
   brew upgrade cmake
+
 else
-  sudo add-apt-repository -y ppa:george-edison55/precise-backports
+
+  # common prerequisites
+  sudo apt-get update -qq
+  sudo apt-get install -y libprotobuf-dev protobuf-compiler cmake
+
   if (( QT4 )); then
-    sudo apt-get update -qq
+    # qt4 prerequisites
     sudo apt-get install -y qtmobility-dev libqt4-dev
   else
-    sudo add-apt-repository -y ppa:beineri/opt-qt542
-    sudo apt-get update -qq
-    sudo apt-get install -y libsqlite3-dev\
-      qt54base qt54webkit qt54tools qt54svg qt54multimedia
+    # qt5 prerequisites
+    sudo apt-get install -y libprotobuf-dev protobuf-compiler \
+      qt5-default qttools5-dev qttools5-dev-tools \
+      qtmultimedia5-dev libqt5multimedia5-plugins libqt5svg5-dev libqt5sql5-mysql
   fi
-  sudo apt-get install -y cmake cmake-data libgtest-dev libprotobuf-dev protobuf-compiler
-  sudo mkdir /usr/src/gtest/build
-  cd /usr/src/gtest/build
-  sudo cmake .. -DBUILD_SHARED_LIBS=1
-  sudo make -j2
-  sudo ln -s /usr/src/gtest/build/libgtest.so /usr/lib/libgtest.so
-  sudo ln -s /usr/src/gtest/build/libgtest_main.so /usr/lib/libgtest_main.so
-  cd -
+
+  # prerequisites for tests
+  if [[ $BUILDTYPE == "Debug" ]]; then
+    if [[ $DIST == "precise" ]]; then
+      sudo add-apt-repository -y ppa:george-edison55/precise-backports
+      sudo apt-get update -qq
+      sudo apt-get install -y cmake cmake-data libgtest-dev
+    else
+      sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
+      sudo apt-get update -qq
+      sudo apt-get install -y cmake cmake-extras libgtest-dev
+    fi
+
+    sudo mkdir /usr/src/gtest/build
+    cd /usr/src/gtest/build
+    sudo cmake .. -DBUILD_SHARED_LIBS=1
+    sudo make -j2
+    sudo ln -s /usr/src/gtest/build/libgtest.so /usr/lib/libgtest.so
+    sudo ln -s /usr/src/gtest/build/libgtest_main.so /usr/lib/libgtest_main.so
+    cd -
+  fi
 fi
