7fe60279de45cbb46f20f4ea33346736a455759f
Fabio Bas <ctrlaltca@gmail.com>
2015-10-08 21:33:24 +0200

    Fix PR #1600

--- travis-compile.sh
+++ travis-compile.sh
@@ -9,11 +9,11 @@ if [[ $TRAVIS_OS_NAME == "osx" && $QT4 == 0 ]]; then
   prefix="-DCMAKE_PREFIX_PATH=`echo /usr/local/Cellar/qt5/5.*/`"
 fi
 if [[ $TRAVIS_OS_NAME == "linux" && $QT4 == 0 ]]; then
-  prefix="-DCMAKE_PREFIX_PATH=/opt/qt52/lib/cmake/"
+  prefix="-DCMAKE_PREFIX_PATH=`echo /opt/qt5*/lib/cmake/`"
 fi
 
 if [[ $BUILDTYPE == "Debug" ]]; then
-	cmake .. -DWITH_SERVER=1 -DCMAKE_BUILD_TYPE=$BUILDTYPE -Dtest=ON -DWITH_QT4=$QT4 $prefix
+	cmake .. -DWITH_SERVER=1 -DWITH_TESTS=1 -DCMAKE_BUILD_TYPE=$BUILDTYPE -DWITH_QT4=$QT4 $prefix
 	make -j2
 	make test
 else
--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -10,16 +10,22 @@ if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
   brew unlink cmake
   brew upgrade cmake
 else
+  sudo add-apt-repository -y ppa:george-edison55/precise-backports
   if (( QT4 )); then
     sudo apt-get update -qq
     sudo apt-get install -y qtmobility-dev libqt4-dev
   else
-    sudo add-apt-repository -y ppa:george-edison55/precise-backports
-    sudo add-apt-repository -y ppa:beineri/opt-qt521
+    sudo add-apt-repository -y ppa:beineri/opt-qt541
     sudo apt-get update -qq
     sudo apt-get install -y libsqlite3-dev\
-      qt52base qt52webkit qt52tools qt52svg qt52multimedia
+      qt54base qt54webkit qt54tools qt54svg qt54multimedia
   fi
-  sudo apt-get install -y cmake libgtest-dev libprotobuf-dev protobuf-compiler
-  cd /usr/src/gtest && sudo cmake . && sudo cmake --build . && sudo mv libg* /usr/local/lib/ ; cd -
+  sudo apt-get install -y cmake cmake-data libgtest-dev libprotobuf-dev protobuf-compiler
+  sudo mkdir /usr/src/gtest/build
+  cd /usr/src/gtest/build
+  sudo cmake .. -DBUILD_SHARED_LIBS=1
+  sudo make -j2
+  sudo ln -s /usr/src/gtest/build/libgtest.so /usr/lib/libgtest.so
+  sudo ln -s /usr/src/gtest/build/libgtest_main.so /usr/lib/libgtest_main.so
+  cd -
 fi
