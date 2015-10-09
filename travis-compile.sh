b63475829b1f7b2855fdb82f38c92078a2327d4c
Fabio Bas <ctrlaltca@gmail.com>
2015-10-09 15:47:34 +0200

    Revert option from WITH_TESTS to TEST

--- travis-compile.sh
+++ travis-compile.sh
@@ -13,7 +13,7 @@ if [[ $TRAVIS_OS_NAME == "linux" && $QT4 == 0 ]]; then
 fi
 
 if [[ $BUILDTYPE == "Debug" ]]; then
-	cmake .. -DWITH_SERVER=1 -DWITH_TESTS=1 -DCMAKE_BUILD_TYPE=$BUILDTYPE -DWITH_QT4=$QT4 $prefix
+	cmake .. -DWITH_SERVER=1 -DTEST=1 -DCMAKE_BUILD_TYPE=$BUILDTYPE -DWITH_QT4=$QT4 $prefix
 	make -j2
 	make test
 else
