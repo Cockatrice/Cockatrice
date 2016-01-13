a9fe0fa7cdb123ad85e77b5a86e10183a3dd454f
Gavin Bisesi <daenyth@gmail.com>
2016-01-13 18:04:43 -0500

    WIP adding ChatView test
    Very rough. Not even trying to get code layout good right now, just want
    it to run. Currently fails to compile

--- tests/dummy_test.cpp
+++ tests/dummy_test.cpp
@@ -1,12 +1,30 @@
 #include "gtest/gtest.h"
+#include "../cockatrice/src/chatview.h"
+
+class QWidget;
 
 namespace {
     class FooTest : public ::testing::Test {
 
     };
 
+    class MockTabSupervisor : TabSupervisor {
+
+        public:
+        MockTabSupervisor(
+                AbstractClient *_client = nullptr,
+                QWidget *parent = nullptr) : TabSupervisor(_client, parent) { }
+    };
+
     TEST(DummyTest, Works) {
         ASSERT_EQ(1, 1) << "One is not equal to one";
+    }
+
+    TEST(ChatViewTest, HandlesUrlTag) {
+        bool showTimestamps = true;
+        auto const *tabSupervisor = new MockTabSupervisor();
+        auto chatView = ChatView(tabSupervisor, nullptr, showTimestamps);
+        // TODO Add some chat messages with [tags] and make sure the result has links in it.
     }
 }
 
