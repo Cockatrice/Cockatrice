937021e7a6c3ebef4d2a891b1e4f9e845a843b97
Kevin Boxhoorn <kevinboxhoorn@yahoo.com>
2016-10-07 11:11:21 +1100

    Clean up `isUsernameValid` function

--- servatrice/src/servatrice_database_interface.cpp
+++ servatrice/src/servatrice_database_interface.cpp
@@ -132,10 +132,12 @@ bool Servatrice_DatabaseInterface::usernameIsValid(const QString &user, QString 
     bool allowNumerics = settingsCache->value("users/allownumerics", true).toBool();
     bool allowPunctuationPrefix = settingsCache->value("users/allowpunctuationprefix", false).toBool();
     QString allowedPunctuation = settingsCache->value("users/allowedpunctuation", "_").toString();
-    QStringList disallowedWords = settingsCache->value("users/disallowedwords", "").toString().split(",", QString::SkipEmptyParts);
+	QString disallowedWordsStr = settingsCache->value("users/disallowedwords", "").toString();
+    QStringList disallowedWords = disallowedWordsStr.split(",", QString::SkipEmptyParts);
     disallowedWords.removeDuplicates();
+	QString disallowedRegExpStr = settingsCache->value("users/disallowedregexp", "").toString();
 
-    error = QString("%1|%2|%3|%4|%5|%6|%7|%8|%9").arg(minNameLength).arg(maxNameLength).arg(allowLowercase).arg(allowUppercase).arg(allowNumerics).arg(allowPunctuationPrefix).arg(allowedPunctuation).arg(settingsCache->value("users/disallowedwords", "").toString()).arg(settingsCache->value("users/disallowedregexp", "").toString());
+    error = QString("%1|%2|%3|%4|%5|%6|%7|%8|%9").arg(minNameLength).arg(maxNameLength).arg(allowLowercase).arg(allowUppercase).arg(allowNumerics).arg(allowPunctuationPrefix).arg(allowedPunctuation).arg(disallowedWordsStr).arg(disallowedRegExpStr);
 
     if (user.length() < minNameLength || user.length() > maxNameLength)
         return false;
