a5006d61dfaaca62a71ff7cbfc9262ea7a8a51a3
woogerboy21 <woogerboy21@gmail.com>
2016-10-25 18:20:42 -0400

    Corrected Typo
    Corrected typo in DB Migration Script

--- servatrice/migrations/servatrice_0017_to_0018.sql
+++ servatrice/migrations/servatrice_0017_to_0018.sql
@@ -1,4 +1,4 @@
--- Servatrice db migration from version 16 to version 18
+-- Servatrice db migration from version 17 to version 18
 
 alter table cockatrice_users add privlevel enum("NONE","VIP","DONATOR") NOT NULL;
 
