4130fe5d388c0212ac514ed70ff90064749a14bd
woogerboy21 <woogerboy21@gmail.com>
2016-12-06 18:31:14 -0500

    Fix Store Replays Functionality
    For whatever reason we have a variable in the servers ini to
    enable/disable the storing of replays but there is no code that uses
    that variable.  At one time there was but in light of it being removed
    some were along the line this add's the ability back in for server
    owners to disable the storing of game replay data.

--- common/server.h
+++ common/server.h
@@ -65,6 +65,7 @@ public:
     virtual bool getRegOnlyServerEnabled() const { return false; }
     virtual bool getMaxUserLimitEnabled() const { return false; }
     virtual bool getEnableLogQuery() const { return false; }
+    virtual bool getStoreReplaysEnabled() const { return true; }
     virtual int getIdleClientTimeout() const { return 0; }
     virtual int getClientKeepAlive() const { return 0; }
     virtual int getMaxGameInactivityTime() const { return 9999999; }
