b09fc7e3a04c9a9f5c5d81e3d1afa56a6b5c2070
woogerboy21 <woogerboy21@gmail.com>
2016-12-07 23:30:18 -0500

    Removed flashy chat

--- cockatrice/src/chatview.cpp
+++ cockatrice/src/chatview.cpp
@@ -160,7 +160,7 @@ void ChatView::appendMessage(QString message, RoomMessageTypeFlags messageType, 
                 const int pixelSize = QFontInfo(cursor.charFormat().font()).pixelSize();
                 QMap<QString, UserListTWI *> buddyList = tabSupervisor->getUserListsTab()->getBuddyList()->getUsers();
                 cursor.insertImage(UserLevelPixmapGenerator::generatePixmap(pixelSize, userLevel,
-                                                                            buddyList.contains(sender)).toImage(), QString::fromStdString(tabSupervisor->getUserInfo()->privlevel()));
+                                                                            buddyList.contains(sender)).toImage());
                 cursor.insertText(" ");
             }
             cursor.setCharFormat(senderFormat);
