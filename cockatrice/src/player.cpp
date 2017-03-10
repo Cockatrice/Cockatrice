be852cb51fd3082b971eba81f4ed047f2d6b1f10
Zach H <Zahalpern+github@gmail.com>
2017-03-10 14:52:38 -0500

    function set

--- cockatrice/src/player.cpp
+++ cockatrice/src/player.cpp
@@ -1083,13 +1083,7 @@ void Player::actCreatePredefinedToken()
     if(!cardInfo)
         return;
 
-    lastTokenName = cardInfo->getName();
-    lastTokenColor = cardInfo->getColors().isEmpty() ? QString() : cardInfo->getColors().first().toLower();
-    lastTokenPT = cardInfo->getPowTough();
-    lastTokenAnnotation = settingsCache->getAnnotateTokens() ? cardInfo->getText() : "";
-    lastTokenTableRow = table->clampValidTableRow(2 - cardInfo->getTableRow());
-    lastTokenDestroy = true;
-    aCreateAnotherToken->setEnabled(true);
+    setLastToken(cardInfo);
 
     actCreateAnotherToken();
 }
@@ -1109,13 +1103,7 @@ void Player::actCreateRelatedCard()
     * then lets allow it to be created via create another
     */
     CardInfo *cardInfo = db->getCard(dbNameFromTokenDisplayName(actionDisplayName));
-    lastTokenName = cardInfo->getName();
-    lastTokenColor = cardInfo->getColors().isEmpty() ? QString() : cardInfo->getColors().first().toLower();
-    lastTokenPT = cardInfo->getPowTough();
-    lastTokenAnnotation = settingsCache->getAnnotateTokens() ? cardInfo->getText() : "";
-    lastTokenTableRow = table->clampValidTableRow(2 - cardInfo->getTableRow());
-    lastTokenDestroy = true;
-    aCreateAnotherToken->setEnabled(true);
+    setLastToken(cardInfo);
 }
 
 void Player::actCreateAllRelatedCards()
@@ -1140,13 +1128,7 @@ void Player::actCreateAllRelatedCards()
     if (relatedCards.length() == 1)
     {
         CardInfo *cardInfo = db->getCard(dbNameFromTokenDisplayName(relatedCards.at(0)));
-        lastTokenName = cardInfo->getName();
-        lastTokenColor = cardInfo->getColors().isEmpty() ? QString() : cardInfo->getColors().first().toLower();
-        lastTokenPT = cardInfo->getPowTough();
-        lastTokenAnnotation = settingsCache->getAnnotateTokens() ? cardInfo->getText() : "";
-        lastTokenTableRow = table->clampValidTableRow(2 - cardInfo->getTableRow());
-        lastTokenDestroy = true;
-        aCreateAnotherToken->setEnabled(true);
+        setLastToken(cardInfo);
     }
 }
 
@@ -2549,4 +2531,15 @@ void Player::processSceneSizeChange(int newPlayerWidth)
 
     table->setWidth(tableWidth);
     hand->setWidth(tableWidth + stack->boundingRect().width());
+}
+
+void Player::setLastToken(CardInfo *cardInfo)
+{
+    lastTokenName = cardInfo->getName();
+    lastTokenColor = cardInfo->getColors().isEmpty() ? QString() : cardInfo->getColors().first().toLower();
+    lastTokenPT = cardInfo->getPowTough();
+    lastTokenAnnotation = settingsCache->getAnnotateTokens() ? cardInfo->getText() : "";
+    lastTokenTableRow = table->clampValidTableRow(2 - cardInfo->getTableRow());
+    lastTokenDestroy = true;
+    aCreateAnotherToken->setEnabled(true);
 }
