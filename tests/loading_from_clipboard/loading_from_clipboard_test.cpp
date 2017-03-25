4940dd1814b7014512217eee361f989d72e46219
Mark McDonald <mark.mcdonald.ga@gmail.com>
2017-03-25 12:27:22 -0400

    Strap decklist creation into the test harness

---
+++ tests/loading_from_clipboard/loading_from_clipboard_test.cpp
@@ -0,0 +1,63 @@
+#include "gtest/gtest.h"
+#include "loading_from_clipboard_test.h"
+#include <QTextStream>
+#include "../../common/decklist.h"
+
+DeckList *fromClipboard(QString *clipboard);
+
+DeckList *fromClipboard(QString *clipboard) {
+    DeckList *deckList = new DeckList;
+    QTextStream *stream = new QTextStream(clipboard);
+    deckList->loadFromStream_Plain(*stream);
+    return deckList;
+}
+
+struct AssertDeckList {
+    QList<QString> expectedCards;
+    int actualDecklistSize = 0;
+
+    AssertDeckList(QList<QString> _expectedCards) : expectedCards(_expectedCards) {}
+
+    void operator()(const InnerDecklistNode *node, const DecklistCardNode *card) {
+        actualDecklistSize += card->getNumber();
+    }
+
+    void assertSizeIsCorrect() {
+        ASSERT_EQ(expectedCards.size(), actualDecklistSize);
+    }
+};
+
+namespace {
+    TEST(LoadingFromClipboardTest, EmptyDeck) {
+        DeckList *deckList = fromClipboard(new QString(""));
+
+        QStringList cardsInDeck = deckList->getCardList();
+        ASSERT_TRUE(cardsInDeck.isEmpty()) << "Deck should be empty";
+    }
+
+    TEST(LoadingFromClipboardTest, EmptySideboard) {
+        DeckList *deckList = fromClipboard(new QString("Sideboard"));
+
+        QStringList cardsInDeck = deckList->getCardList();
+        ASSERT_TRUE(cardsInDeck.isEmpty()) << "Deck should be empty";
+    }
+
+    TEST(LoadingFromClipboardTest, QuantityPrefixed) {
+        QString * clipboard = new QString("");
+        clipboard->append("1 Mountain\n");
+        clipboard->append("2x Island\n");
+        DeckList *deckList = fromClipboard(clipboard);
+
+        QList<QString> expectedDeck = QList<QString>() << "Mountain" << "Island" << "Island";
+        AssertDeckList assertDeckList(expectedDeck);
+
+        deckList->forEachCard(assertDeckList);
+
+        assertDeckList.assertSizeIsCorrect();
+    }
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
