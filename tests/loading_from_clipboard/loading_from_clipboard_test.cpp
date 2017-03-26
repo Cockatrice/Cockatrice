7fca00e9d97944c6c706808bf40110de9b47f90b
Mark McDonald <mark.mcdonald.ga@gmail.com>
2017-03-26 19:09:07 -0400

    Create explicit datastructures for decklist tests

--- tests/loading_from_clipboard/loading_from_clipboard_test.cpp
+++ tests/loading_from_clipboard/loading_from_clipboard_test.cpp
@@ -12,48 +12,93 @@ DeckList *fromClipboard(QString *clipboard) {
     return deckList;
 }
 
+using Cardrows = QMap<QString, int>;
+
 struct AssertDeckList {
-    QList<QString> expectedCards;
-    QList<QString> actualCards;
+    Cardrows expectedMainboard;
+    Cardrows expectedSideboard;
 
-    AssertDeckList(QList<QString> _expectedCards) : expectedCards(_expectedCards) {}
+    Cardrows actualMainboard;
+    Cardrows actualSideboard;
 
-    void operator()(const InnerDecklistNode *, const DecklistCardNode *card) {
-        for (int i = 0; i < card->getNumber(); i++) {
-            actualCards.append(card->getName());
+    AssertDeckList(Cardrows _expectedMainboardMap, Cardrows _expectedSideboard) :
+            expectedMainboard(_expectedMainboardMap), expectedSideboard(_expectedSideboard) {}
+
+    AssertDeckList(Cardrows _expectedMainboardMap) : AssertDeckList(_expectedMainboardMap, Cardrows({})) {}
+
+    void operator()(const InnerDecklistNode *innerDecklistNode, const DecklistCardNode *card) {
+        if (innerDecklistNode->getName() == "main") {
+            actualMainboard[card->getName()] += card->getNumber();
+        } else if (innerDecklistNode->getName() == "side") {
+            actualSideboard[card->getName()] += card->getNumber();
+        } else {
+            FAIL();
         }
     }
 
     void makeAssertions() {
-        ASSERT_EQ(expectedCards.size(), actualCards.size());
-        ASSERT_EQ(expectedCards, actualCards);
+        ASSERT_EQ(expectedSideboard, actualSideboard);
+        ASSERT_EQ(expectedMainboard, actualMainboard);
     }
 };
 
 namespace {
     TEST(LoadingFromClipboardTest, EmptyDeck) {
         DeckList *deckList = fromClipboard(new QString(""));
-
-        QStringList cardsInDeck = deckList->getCardList();
-        ASSERT_TRUE(cardsInDeck.isEmpty()) << "Deck should be empty";
+        ASSERT_TRUE(deckList->getCardList().isEmpty()) << "Deck should be empty";
     }
 
     TEST(LoadingFromClipboardTest, EmptySideboard) {
         DeckList *deckList = fromClipboard(new QString("Sideboard"));
-
-        QStringList cardsInDeck = deckList->getCardList();
-        ASSERT_TRUE(cardsInDeck.isEmpty()) << "Deck should be empty";
+        ASSERT_TRUE(deckList->getCardList().isEmpty()) << "Deck should be empty";
     }
 
     TEST(LoadingFromClipboardTest, QuantityPrefixed) {
-        QString * clipboard = new QString("");
+        QString *clipboard = new QString("");
         clipboard->append("1 Mountain\n");
         clipboard->append("2x Island\n");
         DeckList *deckList = fromClipboard(clipboard);
 
-        QList<QString> expectedDeck = QList<QString>() << "Mountain" << "Island" << "Island";
-        AssertDeckList assertDeckList(expectedDeck);
+        Cardrows expectedMainboard = Cardrows({{"Mountain", 1},
+                                               {"Island",   2}});
+        AssertDeckList assertDeckList(expectedMainboard);
 
+        SCOPED_TRACE("");
+        deckList->forEachCard(assertDeckList);
+
+        assertDeckList.makeAssertions();
+    }
+
+    TEST(LoadingFromClipboardTest, CommentsAreIgnoed) {
+        QString *clipboard = new QString("");
+        clipboard->append("// 1 Mountain\n");
+        clipboard->append("// 2x Island\n");
+        // TODO: Sideboard comments are ignored
+        DeckList *deckList = fromClipboard(clipboard);
+
+        Cardrows expectedMainboard = Cardrows({});
+        AssertDeckList assertDeckList(expectedMainboard);
+
+        SCOPED_TRACE("");
+        deckList->forEachCard(assertDeckList);
+
+        assertDeckList.makeAssertions();
+    }
+
+    TEST(LoadingFromClipboardTest, SideboardPrefix) {
+        QString *clipboard = new QString("");
+        clipboard->append("1 Mountain\n");
+        clipboard->append("SB: 1 Mountain\n");
+        clipboard->append("SB: 2x Island\n");
+        DeckList *deckList = fromClipboard(clipboard);
+
+        Cardrows expectedMainboard = Cardrows({{"Mountain", 1}});
+        Cardrows expectedSideboard = Cardrows({{"Mountain", 1},
+                                               {"Island",   2}});
+
+        AssertDeckList assertDeckList(expectedMainboard, expectedSideboard);
+
+        SCOPED_TRACE("");
         deckList->forEachCard(assertDeckList);
 
         assertDeckList.makeAssertions();
