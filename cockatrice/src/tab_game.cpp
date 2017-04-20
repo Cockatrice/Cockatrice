bf79df69579b5f6822b78a8641305fd084d3d7e2
Zach H <Zahalpern+github@gmail.com>
2017-04-20 03:00:37 -0400

    empty

--- cockatrice/src/tab_game.cpp
+++ cockatrice/src/tab_game.cpp
@@ -755,7 +755,7 @@ void TabGame::processGameEventContainer(const GameEventContainer &cont, Abstract
                 case GameEvent::GAME_CLOSED: eventGameClosed(event.GetExtension(Event_GameClosed::ext), playerId, context); break;
                 case GameEvent::SET_ACTIVE_PLAYER: eventSetActivePlayer(event.GetExtension(Event_SetActivePlayer::ext), playerId, context); break;
                 case GameEvent::SET_ACTIVE_PHASE:
-                    if (sayEdit && sayEdit->text().size() == 0)
+                    if (sayEdit && sayEdit->text().isEmpty())
                         sayEdit->clearFocus();
                     eventSetActivePhase(event.GetExtension(Event_SetActivePhase::ext), playerId, context);
                     break;
