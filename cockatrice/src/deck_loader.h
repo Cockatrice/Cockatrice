b53cd33eedc0366bde4e71b53b8df7139961697a
Zach H <zahalpern+github@gmail.com>
2017-05-14 14:35:40 -0400

    Smarter Clipboard Pasting/Parsing (#2706)

--- cockatrice/src/deck_loader.h
+++ cockatrice/src/deck_loader.h
@@ -37,6 +37,7 @@ protected:
     void saveToStream_DeckZone(QTextStream &out, const InnerDecklistNode *zoneNode);
     void saveToStream_DeckZoneCards(QTextStream &out, const InnerDecklistNode *zoneNode, QList <DecklistCardNode*> cards);
     virtual QString getCardZoneFromName(QString cardName, QString currentZoneName);
+    virtual QString getCompleteCardName(const QString cardName) const;
 };
 
 #endif
