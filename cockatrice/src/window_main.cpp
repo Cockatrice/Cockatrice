2094910b7ae663d5e47a389d7ae33d0774e7acb5
woogerboy21 <woogerboy21@users.noreply.github.com>
2017-06-12 06:12:36 -0400

    More descriptive reason for "to many requests" when registering (#2760)
    * More descriptive reason for "to many requests" when registering
    
    Currently the only way for a user to get a response of "to many registration attempts"  is by the servers configuration to restrict the number of registration attempts but the error message presented to the user is very vague as to why they have been denied.  This PR updates the reasoning.  I'm open for suggestions on any clarity recommendations.
    
    * Added more clarity
    
    Updated error message to include email address information for even greater clarity.

--- cockatrice/src/window_main.cpp
+++ cockatrice/src/window_main.cpp
@@ -453,7 +453,7 @@ void MainWindow::registerError(Response::ResponseCode r, QString reasonStr, quin
             QMessageBox::critical(this, tr("Registration denied"), tr("The email address provider used during registration has been blacklisted for use on this server."));
             break;
         case Response::RespTooManyRequests:
-            QMessageBox::critical(this, tr("Registration denied"), tr("Too many registration attempts, please try again later or contact the server operator for further details."));
+            QMessageBox::critical(this, tr("Registration denied"), tr("It appears you are attempting to register a new account on this server yet you already have an account registered with the email provided. This server restricts the number of accounts a user can register per address.  Please contact the server operator for further assistance or to obtain your credential information."));
             break;
         case Response::RespPasswordTooShort:
             QMessageBox::critical(this, tr("Registration denied"), tr("Password too short."));
