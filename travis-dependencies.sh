26c8380cf36c0e7cd2899481e0a40c2f6d94dc79
tooomm <tooomm@users.noreply.github.com>
2017-07-05 13:18:46 +0200

    cleanup

--- travis-dependencies.sh
+++ travis-dependencies.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
-  brew install ccache                                 # enable caching on mac
-  export PATH="/usr/local/opt/ccache/libexec:$PATH"   # https://docs.travis-ci.com/user/caching/#ccache-on-OS-X
-  #brew update > /dev/null
+  brew install ccache   # enable caching on mac (PATH only set in travis-compile.sh)
   brew install --force qt@5.7
   brew install protobuf
 else
