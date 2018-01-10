823c38de65b0c8586dd9d2451d700afc8921bf97
tooomm <tooomm@users.noreply.github.com>
2018-01-10 22:27:45 +0100

    add placehodler text and clear button
    also 4 spaces to tabs

--- cockatrice/src/filterbuilder.cpp
+++ cockatrice/src/filterbuilder.cpp
@@ -8,76 +8,80 @@
 #include "cardfilter.h"
 
 FilterBuilder::FilterBuilder(QWidget *parent)
-    : QWidget(parent)
+	: QWidget(parent)
 {
-    filterCombo = new QComboBox;
-    filterCombo->setObjectName("filterCombo");
-    for (int i = 0; i < CardFilter::AttrEnd; i++)
-        filterCombo->addItem(
-            tr(CardFilter::attrName(static_cast<CardFilter::Attr>(i))),
-            QVariant(i)
-        );
+	filterCombo = new QComboBox;
+	filterCombo->setObjectName("filterCombo");
+	for (int i = 0; i < CardFilter::AttrEnd; i++)
+		filterCombo->addItem(
+			tr(CardFilter::attrName(static_cast<CardFilter::Attr>(i))),
+			QVariant(i)
+		);
 
-    typeCombo = new QComboBox;
-    typeCombo->setObjectName("typeCombo");
-    for (int i = 0; i < CardFilter::TypeEnd; i++)
-        typeCombo->addItem(
-            tr(CardFilter::typeName(static_cast<CardFilter::Type>(i))),
-            QVariant(i)
-        );
+	typeCombo = new QComboBox;
+	typeCombo->setObjectName("typeCombo");
+	for (int i = 0; i < CardFilter::TypeEnd; i++)
+		typeCombo->addItem(
+			tr(CardFilter::typeName(static_cast<CardFilter::Type>(i))),
+			QVariant(i)
+		);
 
-    QPushButton *ok = new QPushButton(QPixmap("theme:icons/increment"), QString());
-    ok->setObjectName("ok");
-    ok->setMaximumSize(20, 20);
+	QPushButton *ok = new QPushButton(QPixmap("theme:icons/increment"), QString());
+	ok->setObjectName("ok");
+	ok->setMaximumSize(20, 20);
 
-    edit = new QLineEdit;
-    edit->setObjectName("edit");
-    edit->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+	edit = new QLineEdit;
+	edit->setObjectName("edit");
+	edit->setPlaceholderText(tr("Type your filter here"));
+#if QT_VERSION >= 0x050200
+    edit->setClearButtonEnabled(true);
+#endif
+	edit->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
 
-    QGridLayout *layout = new QGridLayout;
-    layout->setObjectName("layout");
-    layout->setContentsMargins(0, 0, 0, 0);
+	QGridLayout *layout = new QGridLayout;
+	layout->setObjectName("layout");
+	layout->setContentsMargins(0, 0, 0, 0);
 
-    layout->addWidget(typeCombo, 0, 0, 1, 2);
-    layout->addWidget(filterCombo, 0, 2, 1, 2);
-    layout->addWidget(edit, 1, 0, 1, 3);
-    layout->addWidget(ok, 1, 3);
+	layout->addWidget(typeCombo, 0, 0, 1, 2);
+	layout->addWidget(filterCombo, 0, 2, 1, 2);
+	layout->addWidget(edit, 1, 0, 1, 3);
+	layout->addWidget(ok, 1, 3);
 
-    setLayout(layout);
+	setLayout(layout);
 
-    connect(edit, SIGNAL(returnPressed()), this, SLOT(emit_add()));
-    connect(ok, SIGNAL(released()), this, SLOT(emit_add()));
-    fltr = NULL;
+	connect(edit, SIGNAL(returnPressed()), this, SLOT(emit_add()));
+	connect(ok, SIGNAL(released()), this, SLOT(emit_add()));
+	fltr = NULL;
 }
 
 FilterBuilder::~FilterBuilder()
 {
-    destroyFilter();
+	destroyFilter();
 }
 
 void FilterBuilder::destroyFilter()
 {
-    if (fltr)
-        delete fltr;
+	if (fltr)
+		delete fltr;
 }
 
 static int comboCurrentIntData(const QComboBox *combo) 
 {
-    return combo->itemData(combo->currentIndex()).toInt();
+	return combo->itemData(combo->currentIndex()).toInt();
 }
 
 void FilterBuilder::emit_add()
 {
-    QString txt;
+	QString txt;
 
-    txt = edit->text();
-    if (txt.length() < 1)
-        return;
+	txt = edit->text();
+	if (txt.length() < 1)
+		return;
 
-    destroyFilter();
-    fltr = new CardFilter(txt,
-                    static_cast<CardFilter::Type>(comboCurrentIntData(typeCombo)),
-                    static_cast<CardFilter::Attr>(comboCurrentIntData(filterCombo)));
-    emit add(fltr);
-    edit->clear();
+	destroyFilter();
+	fltr = new CardFilter(txt,
+					static_cast<CardFilter::Type>(comboCurrentIntData(typeCombo)),
+					static_cast<CardFilter::Attr>(comboCurrentIntData(filterCombo)));
+	emit add(fltr);
+	edit->clear();
 }
