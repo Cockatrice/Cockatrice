4ac7bd7f13a81f21729a3bef3c43fe1250b4cb21
tooomm <tooomm@users.noreply.github.com>
2018-01-25 16:43:01 +0100

    move scripts to separate files

---
+++ .ci/appveyor-create_artifacts.ps1
@@ -0,0 +1,9 @@
+$exe = dir -name *.exe
+$new_name = $exe.Replace(".exe", "-${env:target_arch}_qt5.exe")
+Push-AppveyorArtifact $exe -FileName $new_name
+$cmake_name = $exe.Replace(".exe", "-${env:target_arch}_qt5.cmake.txt")
+Push-AppveyorArtifact CMakeCache.txt -FileName $cmake_name
+$json = New-Object PSObject
+(New-Object PSObject | Add-Member -PassThru NoteProperty bin $new_name | Add-Member -PassThru NoteProperty cmake $cmake_name | Add-Member -PassThru NoteProperty commit $env:APPVEYOR_REPO_COMMIT) | ConvertTo-JSON | Out-File -FilePath "latest-$env:target_arch" -Encoding ASCII
+Push-AppveyorArtifact "latest-$env:target_arch"
+$version = $matches['content']
