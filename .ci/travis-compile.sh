994a643d9c4e08dad1dfd7b13272598fdb42b196
ctrlaltca <ctrlaltca@gmail.com>
2018-01-27 19:43:48 +0100

    Travis: enable ccache usage under macOS; halves compilation time (#3052)

--- .ci/travis-compile.sh
+++ .ci/travis-compile.sh
@@ -9,12 +9,11 @@ cd build
 prefix=""
 
 if [[ $TRAVIS_OS_NAME == "osx" ]]; then
-  export PATH="/usr/local/opt/ccache/libexec:$PATH"
+  export PATH="/usr/local/opt/ccache/bin:$PATH"
   prefix="-DCMAKE_PREFIX_PATH=$(echo /usr/local/opt/qt*/)"
 fi
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
   prefix="-DCMAKE_PREFIX_PATH=$(echo /opt/qt5*/lib/cmake/)"
-  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$(echo /opt/qt5*/lib/)"
 fi
 
 if [[ $BUILDTYPE == "Debug" ]]; then
