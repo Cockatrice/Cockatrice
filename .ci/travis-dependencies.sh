95477442f539d10333bf9c63d5729c17b72473ac
Fabio Bas <ctrlaltca@gmail.com>
2018-01-27 18:01:50 +0100

    wtf++

--- .ci/travis-dependencies.sh
+++ .ci/travis-dependencies.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
-  brew install ccache   # enable caching on mac (PATH only set in travis-compile.sh)
   brew install --force qt@5.7
-  brew install protobuf clang-format
+  brew install protobuf clang-format ccache
 fi
 if [[ $TRAVIS_OS_NAME == "linux" ]] ; then
   echo Skipping... packages are installed with the Travis apt addon for sudo disabled container builds
