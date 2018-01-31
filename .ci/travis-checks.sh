3cfe83fc5300bebda8536dc81aa9ad5de9ec2e5a
tooomm <tooomm@users.noreply.github.com>
2018-01-31 19:42:35 +0100

    test /4

--- .ci/travis-checks.sh
+++ .ci/travis-checks.sh
@@ -2,7 +2,7 @@
 
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
   if [[ $BUILDTYPE == "Debug" ]]; then #TODO: generalize h cpp files search to top-directory? / check with ctrlaltca
-    echo "Checking code style..."
+    echo "Checking code style...";
     clang-format -i \
       common/*.h \
       common/*.cpp \
@@ -14,7 +14,7 @@ if [[ $TRAVIS_OS_NAME == "linux" ]]; then
       servatrice/src/*.cpp
       
     git clean -f
-    git diff --quiet && echo "Code style - OK" || (
+    git diff --quiet && echo "Code style - OK"; || (
       echo "*****************************************************";
       echo "***  This PR is not clean against our code style  ***";
       echo "***  Run clang-format and fix up any differences  ***";
@@ -24,9 +24,9 @@ if [[ $TRAVIS_OS_NAME == "linux" ]]; then
     )
     git diff --exit-code
   else
-    echo "Skipping... checks are only performed on runs in debug mode"
+    echo "Skipping... checks are only performed on runs in debug mode";
 fi
 
 if [[ $TRAVIS_OS_NAME == "osx" ]]; then
-  echo "Skipping... checks are only performed on runs from Linux"
+  echo "Skipping... checks are only performed on runs from Linux";
 fi
