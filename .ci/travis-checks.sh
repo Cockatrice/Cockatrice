61c263cfa0fa7e50a1865417064a854a1b2231cf
tooomm <tooomm@users.noreply.github.com>
2018-02-06 16:25:18 +0100

    clangify test :)

--- .ci/travis-checks.sh
+++ .ci/travis-checks.sh
@@ -1,33 +1,9 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-  if [[ $BUILDTYPE == "Debug" ]]; then   ## TODO: generalize h cpp files search to top-directory? run bash script by zach here?
-    echo "Checking code style..."
-    clang-format -i \
-      common/*.h \
-      common/*.cpp \
-      cockatrice/src/*.h \
-      cockatrice/src/*.cpp \
-      oracle/src/*.h \
-      oracle/src/*.cpp \
-      servatrice/src/*.h \
-      servatrice/src/*.cpp
-      
-    git clean -f
-    if git diff --quiet; then
-      ## exit code = 0 (no differences)
-      echo "Code style - OK"
-    else (
-      ## exit code = 1 (differences)
-      echo "*****************************************************";
-      echo "***  This PR is not clean against our code style  ***";
-      echo "***  Run clang-format and fix up any differences  ***";
-      echo "***  Check our CONTRIBUTING.md file for details!  ***";
-      echo "***                  Thank you ♥                  ***";
-      echo "*****************************************************";
-      )
-    fi
-    git diff --exit-code   ## correct position?
+  if [[ $BUILDTYPE == "Debug" ]]; then
+    ## Check code style
+	../clangify.sh
   else
     echo "Skipping... checks are only performed on runs in debug mode"
   fi
--- clangify.sh
+++ clangify.sh
@@ -4,6 +4,12 @@
 
 set -e
 
+if [[ $TRAVIS == true ]]; then
+    echo "Checking code style of repository..."
+else
+    echo "Applying code style to repository..."
+fi
+
 if hash clang-format 2>/dev/null; then
 	find . \( -name "*.cpp" -o -name "*.h" \) \
 		-not -path "./cockatrice/src/qt-json/*" \
@@ -12,7 +18,26 @@ if hash clang-format 2>/dev/null; then
 		-not -path "./oracle/src/zip/*" \
 		-not -path "./build*/*" \
 		-exec clang-format -style=file -i {} \;
-	echo "Repository properly formatted"
+
+		if [[ $TRAVIS == false ]]; then
+            echo "Repository properly formatted"
+		else
+			git clean -f
+			if git diff --quiet; then
+				## exit code = 0 (no differences)
+				echo "Code style - OK"   ## 
+			else (
+				## exit code = 1 (differences)
+				echo "*****************************************************";
+				echo "***  This PR is not clean against our code style  ***";
+				echo "***  Run clang-format and fix up any differences  ***";
+				echo "***  Check our CONTRIBUTING.md file for details!  ***";
+				echo "***                  Thank you ♥                  ***";
+				echo "*****************************************************";
+				)
+			fi
+        fi
+
 else
-	echo "Please install clang-format to use this program"
+	echo "Please install clang-format to use this program! Read our CONTRIBUTING.md file for details."			
 fi
