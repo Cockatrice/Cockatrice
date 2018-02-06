59f2f2706a238166bf3ca7419b36588361260379
tooomm <tooomm@users.noreply.github.com>
2018-02-07 00:07:33 +0100

    fix indentation

--- .ci/travis-checks.sh
+++ .ci/travis-checks.sh
@@ -2,19 +2,19 @@
 
 if [[ $TRAVIS == true ]]; then
 
-  if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-    if [[ $BUILDTYPE == "Debug" ]]; then
-      echo "Checking code style of repository..."
-      ./clangify.sh
-    else
-      echo "Skipping... checks are only performed on runs in debug mode"
+    if [[ $TRAVIS_OS_NAME == "linux" ]]; then
+        if [[ $BUILDTYPE == "Debug" ]]; then
+            echo "Checking code style..."
+            ./clangify.sh
+        else
+            echo "Skipping... checks are only performed on runs in debug mode"
+        fi
     fi
-  fi
-
-  if [[ $TRAVIS_OS_NAME == "osx" ]]; then
-    echo "Skipping... checks are only performed on runs from Linux"
-  fi
+    
+    if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+        echo "Skipping... checks are only performed on runs from Linux"
+    fi
 
 else
-  echo "Doing nothing. This script is supposed to be run by Travis CI only!"
+    echo "Doing nothing. This script is supposed to be run by Travis CI only!"
 fi
--- .ci/travis-dependencies.sh
+++ .ci/travis-dependencies.sh
@@ -2,14 +2,14 @@
 
 if [[ $TRAVIS == true ]]; then
 
-  if [[ $TRAVIS_OS_NAME == "linux" ]] ; then
-    echo "Skipping... Linux packages are installed with the apt addon for sudo disabled container builds as configuered in travis.yml"
-  fi
-  if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
-    brew update
-    brew install ccache protobuf qt@5.5
-  fi
+    if [[ $TRAVIS_OS_NAME == "linux" ]] ; then
+        echo "Skipping... Linux packages are installed with the apt addon for sudo disabled container builds as configuered in travis.yml"
+    fi
+    if [[ $TRAVIS_OS_NAME == "osx" ]] ; then
+        brew update
+        brew install ccache protobuf qt@5.5
+    fi
 
 else
-  echo "Doing nothing. This script is supposed to be run by Travis CI only!"
+    echo "Doing nothing. This script is supposed to be run by Travis CI only!"
 fi
