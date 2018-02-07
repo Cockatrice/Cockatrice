5550aa9d37764bf92a468f09ec738c9602c24d67
Zach Halpern <Zahalpern+github@gmail.com>
2018-02-07 11:36:16 -0500

    const

--- cockatrice/src/filtertree.cpp
+++ cockatrice/src/filtertree.cpp
@@ -236,7 +236,7 @@ bool FilterItem::acceptCmc(const CardInfoPtr info) const
     // if conversion failed, check for the "//" separator used in split cards
     if (!convertSuccess) {
         int cmcSum = 0;
-        for (QString cmc : info->getCmc().split("//")) {
+        for (const QString &cmc : info->getCmc().split("//")) {
             cmcInt = cmc.toInt();
             cmcSum += cmcInt;
             if (relationCheck(cmcInt)) {
