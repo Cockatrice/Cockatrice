c8a10a99979929ae6fc115ce5ceaa5f7367be6bf
Vafthrudnir <szd0517@gmail.com>
2018-02-14 02:40:46 +0100

    Mnemonic created

--- oracle/src/oraclewizard.cpp
+++ oracle/src/oraclewizard.cpp
@@ -505,7 +505,7 @@ void SaveSetsPage::retranslateUi()
                    "Press \"Save\" to save the imported cards to the Cockatrice database."));
 
     defaultPathCheckBox->setText(tr("Save to the default path (recommended)"));
-    setButtonText(QWizard::NextButton, tr("Save"));
+    setButtonText(QWizard::NextButton, tr("&Save"));
 }
 
 void SaveSetsPage::updateTotalProgress(int cardsImported, int /* setIndex */, const QString &setName)
