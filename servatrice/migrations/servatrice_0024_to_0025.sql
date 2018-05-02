b79530dd6b76ddd1437d5aa3fe89be7d7cf57c65
Zach H <zahalpern+github@gmail.com>
2018-05-02 14:53:27 -0400

    Update servatrice_0024_to_0025.sql

--- servatrice/migrations/servatrice_0024_to_0025.sql
+++ servatrice/migrations/servatrice_0024_to_0025.sql
@@ -3,4 +3,4 @@
 ALTER TABLE cockatrice_uptime ADD COLUMN mods_count int(11) NOT NULL DEFAULT 0;
 ALTER TABLE cockatrice_uptime ADD COLUMN mods_list TEXT;
 
-UPDATE cockatrice_schema_version SET version = 25 WHERE version = 24;
+UPDATE cockatrice_schema_version SET version=25 WHERE version=24;
