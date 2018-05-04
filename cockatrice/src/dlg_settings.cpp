4e10ce7473b925b24bce81d53c63cd5ad4644ce5
tooomm <tooomm@users.noreply.github.com>
2018-05-04 09:48:53 +0200

    smaller clear pic button (#3220)

--- cockatrice/src/dlg_settings.cpp
+++ cockatrice/src/dlg_settings.cpp
@@ -102,7 +102,7 @@ GeneralSettingsPage::GeneralSettingsPage()
     personalGrid->addWidget(&fallbackUrlLabel, 7, 0, 1, 1);
     personalGrid->addWidget(fallbackUrlEdit, 7, 1, 1, 1);
     personalGrid->addWidget(&fallbackUrlRestoreButton, 7, 2, 1, 1);
-    personalGrid->addWidget(&clearDownloadedPicsButton, 8, 0, 1, 3);
+    personalGrid->addWidget(&clearDownloadedPicsButton, 8, 1);
 
     urlLinkLabel.setTextInteractionFlags(Qt::LinksAccessibleByMouse);
     urlLinkLabel.setOpenExternalLinks(true);
