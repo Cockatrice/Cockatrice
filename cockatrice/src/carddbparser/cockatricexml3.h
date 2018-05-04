a7f4aace9c5704d97fce77ab5c2f37badb5ef77c
ctrlaltca <ctrlaltca@gmail.com>
2018-05-04 09:46:08 +0200

    Extract the xml parser from carddatabase (#3223)

---
+++ cockatrice/src/carddbparser/cockatricexml3.h
@@ -0,0 +1,36 @@
+#ifndef COCKATRICE_XML3_H
+#define COCKATRICE_XML3_H
+
+#include <QXmlStreamReader>
+
+#include "carddatabaseparser.h"
+
+class CockatriceXml3Parser : public ICardDatabaseParser
+{
+    Q_OBJECT
+    Q_INTERFACES(ICardDatabaseParser)
+public:
+    CockatriceXml3Parser() = default;
+    ~CockatriceXml3Parser() = default;
+    bool getCanParseFile(const QString &name, QIODevice &device);
+    void parseFile(QIODevice &device);
+    bool saveToFile(SetNameMap sets, CardNameMap cards, const QString &fileName);
+
+private:
+    /*
+     * A cached list of the available sets, needed to cross-reference sets from cards.
+     */
+    SetNameMap sets;
+
+    CardSetPtr internalAddSet(const QString &setName,
+                              const QString &longName = "",
+                              const QString &setType = "",
+                              const QDate &releaseDate = QDate());
+    void loadCardsFromXml(QXmlStreamReader &xml);
+    void loadSetsFromXml(QXmlStreamReader &xml);
+signals:
+    void addCard(CardInfoPtr card);
+    void addSet(CardSetPtr set);
+};
+
+#endif
