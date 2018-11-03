95676e61d97114d421c8e9f9a07a5f9f372278a2
ebbit1q <ebbit1q@gmail.com>
2018-11-03 22:18:31 +0100

    add old QHash version support
    QT 5.5 does not support using initializer lists for QHash.
    Implement a preprocessor version check for conditionally using [] access
    instead of a const with initializer list.

--- common/decklist.cpp
+++ common/decklist.cpp
@@ -489,11 +489,21 @@ bool DeckList::loadFromStream_Plain(QTextStream &in)
     const QRegularExpression reBrace(" ?[\\[\\{][^\\]\\}]*[\\]\\}] ?"); // not nested
     const QRegularExpression reRoundBrace("^\\([^\\)]*\\) ?");          // () are only matched at start of string
     const QRegularExpression reDigitBrace(" ?\\(\\d*\\) ?");            // () are matched if containing digits
+#if QT_VERSION < 0x050600
+    // this is outdated, current QT versions support initializer lists
+    QHash<QRegularExpression, QString> differences;
+    differences[QRegularExpression("’")] = QString("'");
+    differences[QRegularExpression("Æ")] = QString("Ae");
+    differences[QRegularExpression("æ")] = QString("ae");
+    differences[QRegularExpression(" ?[|/]+ ?")] = QString(" // ");
+    differences[QRegularExpression("(?<![A-Z]) ?& ?")] = QString(" // ");
+#else
     const QHash<QRegularExpression, QString> differences{{QRegularExpression("’"), QString("'")},
                                                          {QRegularExpression("Æ"), QString("Ae")},
                                                          {QRegularExpression("æ"), QString("ae")},
                                                          {QRegularExpression(" ?[|/]+ ?"), QString(" // ")},
                                                          {QRegularExpression("(?<![A-Z]) ?& ?"), QString(" // ")}};
+#endif
 
     cleanList();
 
