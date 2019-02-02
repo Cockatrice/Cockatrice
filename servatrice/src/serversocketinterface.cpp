af935bee85fc58da48d73c3931729729e171555d
Zach Halpern <ZaHalpern+github@gmail.com>
2019-02-02 14:01:42 -0500

    fix check
    Signed-off-by: Zach Halpern <ZaHalpern+github@gmail.com>

--- servatrice/src/serversocketinterface.cpp
+++ servatrice/src/serversocketinterface.cpp
@@ -1037,15 +1037,14 @@ Response::ResponseCode AbstractServerSocketInterface::cmdRegisterAccount(const C
         return Response::RespUserAlreadyExists;
     }
 
-    if ((sqlInterface->checkNumberOfUserAccounts(emailAddress) >= servatrice->getMaxAccountsPerEmail())) {
-        if (servatrice->getMaxAccountsPerEmail() != 0) {
-            if (servatrice->getEnableRegistrationAudit())
-                sqlInterface->addAuditRecord(QString::fromStdString(cmd.user_name()).simplified(), this->getAddress(),
-                                             QString::fromStdString(cmd.clientid()).simplified(), "REGISTER_ACCOUNT",
-                                             "Too many usernames registered with this email address", false);
+    if (servatrice->getMaxAccountsPerEmail() > 0 &&
+        sqlInterface->checkNumberOfUserAccounts(emailAddress) >= servatrice->getMaxAccountsPerEmail()) {
+        if (servatrice->getEnableRegistrationAudit())
+            sqlInterface->addAuditRecord(QString::fromStdString(cmd.user_name()).simplified(), this->getAddress(),
+                                         QString::fromStdString(cmd.clientid()).simplified(), "REGISTER_ACCOUNT",
+                                         "Too many usernames registered with this email address", false);
 
-            return Response::RespTooManyRequests;
-        }
+        return Response::RespTooManyRequests;
     }
 
     QString banReason;
