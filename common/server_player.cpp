4c79987718c855dc8b3007363866f3fa1faa9bdd
ebbit1q <ebbit1q@gmail.com>
2019-03-04 03:47:49 +0100

    don't remove pt! 🔥🔥🔥🔥

--- common/server_player.cpp
+++ common/server_player.cpp
@@ -586,12 +586,6 @@ Response::ResponseCode Server_Player::moveCard(GameEventStorage &ges,
                 setCardAttrHelper(ges, targetzone->getPlayer()->getPlayerId(), targetzone->getName(), card->getId(),
                                   AttrTapped, "1");
             }
-            QString ptString = QString::fromStdString(thisCardProperties->pt());
-            if (!faceDown) {
-                ptString = QString::fromStdString(thisCardProperties->pt());
-            }
-            setCardAttrHelper(ges, targetzone->getPlayer()->getPlayerId(), targetzone->getName(), card->getId(), AttrPT,
-                              ptString);
         }
         if (startzone->getAlwaysRevealTopCard() && !startzone->getCards().isEmpty() && (originalPosition == 0)) {
             Event_RevealCards revealEvent;
