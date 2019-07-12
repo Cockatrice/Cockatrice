7f98fa259b095fa7bed916671d2f12a2ba0f5e8a
tooomm <tooomm@users.noreply.github.com>
2019-07-12 22:45:21 +0200

    Create travis-lint.sh

---
+++ .ci/travis-lint.sh
@@ -0,0 +1,36 @@
+# Check formatting using clang-format
+echo "Checking your code using clang-format..."
+diff="$(./clangify.sh --diff --cf-version)"
+err=$?
+case $err in
+  1)
+    cat <<EOM
+***********************************************************
+***                                                     ***
+***    Your code does not comply with our styleguide.   ***
+***                                                     ***
+***  Please correct it or run the "clangify.sh" script. ***
+***  Then commit and push those changes to this branch. ***
+***   Check our CONTRIBUTING.md file for more details.  ***
+***                                                     ***
+***                     Thank you ♥                     ***
+***                                                     ***
+***********************************************************
+Used clang-format version:
+${diff%%
+*}
+The following changes should be made:
+${diff#*
+}
+Exiting...
+EOM
+      exit 2
+      ;;
+    0)
+      echo "Thank you for complying with our code standards."
+      ;;
+    *)
+      echo "Something went wrong in our formatting checks: clangify returned $err" >&2
+      ;;
+  esac
+fi
