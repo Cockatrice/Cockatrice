b072b540a25bd031236d3faa0caaf3fc54576a46
tooomm <tooomm@users.noreply.github.com>
2019-11-23 05:53:47 +0100

    Travis: more appealing thank you message (#3858)

--- .ci/travis-lint.sh
+++ .ci/travis-lint.sh
@@ -2,36 +2,58 @@
 
 # Check formatting using clang-format
 echo "Checking your code using clang-format..."
+
 diff="$(./clangify.sh --diff --cf-version)"
 err=$?
+
 case $err in
   1)
     cat <<EOM
+
 ***********************************************************
 ***                                                     ***
-***    Your code does not comply with our styleguide.   ***
+***   Your code does not comply with our style guide.   ***
 ***                                                     ***
 ***  Please correct it or run the "clangify.sh" script. ***
 ***  Then commit and push those changes to this branch. ***
 ***   Check our CONTRIBUTING.md file for more details.  ***
 ***                                                     ***
-***                     Thank you ♥                     ***
+***                    Thank you ❤️                     ***
 ***                                                     ***
 ***********************************************************
+
 Used clang-format version:
 ${diff%%
 *}
+
 The following changes should be made:
 ${diff#*
 }
+
 Exiting...
 EOM
       exit 2
       ;;
+
     0)
-      echo "Thank you for complying with our code standards."
+      cat <<EOM
+
+***********************************************************
+***                                                     ***
+***       Your code complies with our style guide!      ***
+***                                                     ***
+***                      Awesome 👍                     ***
+***                                                     ***
+***********************************************************
+
+Exiting...
+EOM
+      exit 0
       ;;
+
     *)
+      echo ""
       echo "Something went wrong in our formatting checks: clangify returned $err" >&2
+      echo ""
       ;;
   esac
