aa2a7fd278327e96c93602b37b8cb6eb3efaff79
ebbit1q <ebbit1q@gmail.com>
2020-05-17 00:50:07 +0200

    use translation map

--- cockatrice/src/capitalizecountername.h
+++ cockatrice/src/capitalizecountername.h
@@ -6,36 +6,19 @@
 
 class CapitalizeCounterName
 {
+    Q_DECLARE_TR_FUNCTIONS(CapitalizeCounterName)
+
+    static const QMap<QString, QString> translated;
 
 public:
-    static QString getDisplayName(QString _name)
+    static QString getDisplayName(const QString &name)
     {
-        if (_name == "life") {
-            return QString("Life");
+        if (translated.contains(name)) {
+            return tr(translated[name].toLatin1());
+        } else {
+            return name;
         }
-        if (_name == "w") {
-            return QString("W");
-        }
-        if (_name == "u") {
-            return QString("U");
-        }
-        if (_name == "b") {
-            return QString("B");
-        }
-        if (_name == "r") {
-            return QString("R");
-        }
-        if (_name == "g") {
-            return QString("G");
-        }
-        if (_name == "x") {
-            return QString("C");
-        }
-        if (_name == "storm") {
-            return QString("Storm");
-        }
-        return QString("Unknown");
     }
 };
 
-#endif
+#endif // CAPITALIZECOUNTERNAME_H
