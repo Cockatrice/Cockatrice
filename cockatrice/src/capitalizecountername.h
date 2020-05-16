42cc326df39553c842daaf933be866d95a9e6d01
Henning Odén <henning.oden@outlook.com>
2020-05-16 21:09:03 +0200

    Merge commit with different approach

---
+++ cockatrice/src/capitalizecountername.h
@@ -0,0 +1,40 @@
+#ifndef CAPITALIZECOUNTERNAME_H
+#define CAPITALIZECOUNTERNAME_H
+
+#include <QString>
+#include <QtCore>
+
+class CapitalizeCounterName
+{
+public:
+    static QString getDisplayName(QString _name)
+    {
+        if (_name == "life") {
+            return QString("Life");
+        }
+        if (_name == "w") {
+            return QString("W");
+        }
+        if (_name == "u") {
+            return QString("U");
+        }
+        if (_name == "b") {
+            return QString("B");
+        }
+        if (_name == "r") {
+            return QString("R");
+        }
+        if (_name == "g") {
+            return QString("G");
+        }
+        if (_name == "x") {
+            return QString("C");
+        }
+        if (_name == "storm") {
+            return QString("Storm");
+        }
+        return QString("Unknown");
+    }
+};
+
+#endif
