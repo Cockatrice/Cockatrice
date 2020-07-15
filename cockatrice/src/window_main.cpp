5762315c90d56d12da596feaae8e49292b7b0591
ebbit1q <ebbit1q@gmail.com>
2020-07-15 15:39:24 +0200

    make opening directories the same for all oses

--- cockatrice/src/window_main.cpp
+++ cockatrice/src/window_main.cpp
@@ -1193,42 +1193,13 @@ void MainWindow::notifyUserAboutUpdate()
 void MainWindow::actOpenCustomFolder()
 {
     QString dir = settingsCache->getCustomPicsPath();
-#if defined(Q_OS_MAC)
-    QStringList scriptArgs;
-    scriptArgs << QLatin1String("-e");
-    scriptArgs << QString::fromLatin1(R"(tell application "Finder" to open POSIX file "%1")").arg(dir);
-    scriptArgs << QLatin1String("-e");
-    scriptArgs << QLatin1String("tell application \"Finder\" to activate");
-
-    QProcess::execute("/usr/bin/osascript", scriptArgs);
-#elif defined(Q_OS_WIN)
-    QStringList args;
-    args << QDir::toNativeSeparators(dir);
-    QProcess::startDetached("explorer", args);
-#else // LINUX
     QDesktopServices::openUrl(QUrl::fromLocalFile(dir));
-#endif
 }
 
 void MainWindow::actOpenCustomsetsFolder()
 {
     QString dir = settingsCache->getCustomCardDatabasePath();
-
-#if defined(Q_OS_MAC)
-    QStringList scriptArgs;
-    scriptArgs << QLatin1String("-e");
-    scriptArgs << QString::fromLatin1(R"(tell application "Finder" to open POSIX file "%1")").arg(dir);
-    scriptArgs << QLatin1String("-e");
-    scriptArgs << QLatin1String("tell application \"Finder\" to activate");
-
-    QProcess::execute("/usr/bin/osascript", scriptArgs);
-#elif defined(Q_OS_WIN)
-    QStringList args;
-    args << QDir::toNativeSeparators(dir);
-    QProcess::startDetached("explorer", args);
-#else // LINUX
     QDesktopServices::openUrl(QUrl::fromLocalFile(dir));
-#endif
 }
 
 void MainWindow::actAddCustomSet()
