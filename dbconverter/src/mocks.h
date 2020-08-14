daa89a9fb4236c8a892f6a7778266534907a985b
olegshtch <olegshtch@yandex.ru>
2020-08-14 19:45:15 +0300

    Run tests on WIndows CI (#4056)
    * Run tests on Windows CI.
    
    * Add message logger
    
    * Skip tests that cannot be linked.
    
    * Fix test call
    
    * Fix mock link issue on MSVC
    
    * Fix PATH variable to find libraries for tests
    
    * Fail test step on test errors

--- dbconverter/src/mocks.h
+++ dbconverter/src/mocks.h
@@ -1,6 +1,8 @@
 /*
  * Beware of this preprocessor hack used to redefine the settingCache class
  * instead of including it and all of its dependencies.
+ * Always set header guards of mocked objects before including any headers
+ * with mocked objects.
  */
 
 #include <QObject>
