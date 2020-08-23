191f4e40c762c08b0e7869ee83de9d7dffce5156
ebbit1q <ebbit1q@gmail.com>
2020-08-23 21:50:19 +0200

    Werror=reorder warning

--- cockatrice/src/player.cpp
+++ cockatrice/src/player.cpp
@@ -95,8 +95,8 @@ void PlayerArea::setSize(qreal width, qreal height)
 Player::Player(const ServerInfo_User &info, int _id, bool _local, bool _judge, TabGame *_parent)
     : QObject(_parent), game(_parent), shortcutsActive(false), defaultNumberTopCards(1),
       defaultNumberTopCardsToPlaceBelow(1), lastTokenDestroy(true), lastTokenTableRow(0), id(_id), active(false),
-      local(_local), judge(_judge), mirrored(false), handVisible(false), conceded(false), dialogSemaphore(false),
-      deck(nullptr), zoneId(0)
+      local(_local), judge(_judge), mirrored(false), handVisible(false), conceded(false), zoneId(0),
+      dialogSemaphore(false), deck(nullptr)
 {
     userInfo = new ServerInfo_User;
     userInfo->CopyFrom(info);
