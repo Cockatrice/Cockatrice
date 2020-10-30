4de9f0c3200f7edb887676d70cfa1206003299e7
tooomm <tooomm@users.noreply.github.com>
2020-10-30 21:33:02 +0100

    Update dlg_creategame.cpp

--- cockatrice/src/dlg_creategame.cpp
+++ cockatrice/src/dlg_creategame.cpp
@@ -95,7 +95,7 @@ void DlgCreateGame::sharedCtor()
     grid->addWidget(generalGroupBox, 0, 0);
     grid->addWidget(joinRestrictionsGroupBox, 0, 1);
     grid->addWidget(gameTypeGroupBox, 1, 0);
-    grid->addWidget(spectatorsGroupBox, 1, 1, 2);
+    grid->addWidget(spectatorsGroupBox, 1, 1, Qt::AlignTop);
     grid->addWidget(rememberGameSettings, 2, 0);
 
     buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok);
