410f73ef7b122d9bcae379ec159c0a31f0d59115
ebbit1q <ebbit1q@gmail.com>
2021-04-27 05:01:24 +0200

    don't reset pt if there is nothing to reset
    when the client resets the pt of a card it intentionally does not
    include cards that already have the correct pt, this can lead to the
    client sending an empty command to the server, which will be rejected

--- cockatrice/src/player.cpp
+++ cockatrice/src/player.cpp
@@ -2984,7 +2984,9 @@ void Player::actResetPT()
         }
     }
 
-    game->sendGameCommand(prepareGameCommand(commandList), playerid);
+    if (!commandList.empty()){
+        game->sendGameCommand(prepareGameCommand(commandList), playerid);
+    }
 }
 
 QVariantList Player::parsePT(const QString &pt)
