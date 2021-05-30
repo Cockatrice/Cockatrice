1ab8274bafcd6dcce113cfb514167a080f9c06ea
ebbit1q <ebbit1q@gmail.com>
2021-05-30 02:13:36 +0200

    add workaround for old qt versions

--- cockatrice/src/chatview/chatview.h
+++ cockatrice/src/chatview/chatview.h
@@ -21,6 +21,9 @@ class TabGame;
 class UserMessagePosition
 {
 public:
+#if (QT_VERSION < QT_VERSION_CHECK(5, 13, 0))
+    UserMessagePosition() = default; // older qt versions require a default constructor to use in containers
+#endif
     UserMessagePosition(QTextCursor &cursor);
     int relativePosition;
     QTextBlock block;
