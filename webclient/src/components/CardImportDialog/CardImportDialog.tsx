f6816451bfb2e06d7a45e770d137d1143517c43c
Jeremy Letto <jeremy.letto@datasite.com>
2021-07-13 22:50:56 -0500

    begin stepper component

--- webclient/src/components/CardImportDialog/CardImportDialog.tsx
+++ webclient/src/components/CardImportDialog/CardImportDialog.tsx
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
-import Dialog from "@material-ui/core/Dialog";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Typography from '@material-ui/core/Typography';
 
 import { CardImportForm } from 'forms';
 
-interface CardImportDialogProps {
-  isOpen: boolean;
-  handleClose: Function;
-}
+import './CardImportDialog.css';
 
-const CardImportDialog = ({ handleClose, isOpen }: CardImportDialogProps) => {
+const CardImportDialog = ({ classes, handleClose, isOpen }: any) => {
   const handleOnClose = () => {
     handleClose();
   }
 
   return (
     <Dialog onClose={handleOnClose} open={isOpen}>
-      <CardImportForm></CardImportForm>
+      <DialogTitle disableTypography className="dialog-title">
+        <Typography variant="h6">Import Cards</Typography>
+
+        {handleOnClose ? (
+          <IconButton onClick={handleOnClose}>
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+      <DialogContent>
+        <CardImportForm></CardImportForm>
+      </DialogContent>
     </Dialog>
   );
 };
--- webclient/src/forms/CardImportForm/CardImportForm.tsx
+++ webclient/src/forms/CardImportForm/CardImportForm.tsx
@@ -10,20 +10,77 @@ import StepLabel from '@material-ui/core/StepLabel';
 
 import { InputField } from "components";
 import { cardImporterService } from 'services';
-import { FormKey, KnownHost, KnownHosts } from 'types';
+import { FormKey } from 'types';
 
 import "./CardImportForm.css";
 import { Select, MenuItem } from "@material-ui/core";
 
 const CardImportForm = (props) => {
   const { handleSubmit, dispatch } = props;
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = ['Source selection', 'Sets imported', 'Import tokens', 'Finished'];
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const getStepContent = (stepIndex) => {
+    switch (stepIndex) {
+      case 0: return (
+        <Form className="cardImportForm" onSubmit={handleSubmit}>
+          <div className="cardImportForm-item">
+            <Field label="Download URL" name="cardDownloadUrl" component={InputField} />
+          </div>
+
+          <Button color="primary" onClick={handleNext}>Import</Button>
+        </Form>
+      );
+
+      case 1: return (
+        <div>
+          <div>sets imported</div>
+          <Button onClick={handleBack}>Go Back</Button>
+          <Button onClick={handleNext}>Save</Button>
+        </div>
+      );
+
+      case 2: return (
+        <Form className="cardImportForm" onSubmit={handleSubmit}>
+          <div className="cardImportForm-item">
+            <Field label="Download URL" name="tokenDownloadUrl" component={InputField} />
+          </div>
+
+          <Button onClick={handleBack}>Go Back</Button>
+          <Button onClick={handleNext}>Import</Button>
+        </Form>
+      );
+
+      case 3: return (
+        <div>
+          <div>Finished!</div>
+
+          <Button onClick={handleBack}>Go Back</Button>
+        </div>
+      );
+    }
+  };
 
   return (
-    <Form className="cardImportForm" onSubmit={handleSubmit}>
-      <div className="cardImportForm-item">
-        <Field label="Download URL" name="download" component={InputField} />
-      </div>
-    </Form>
+    <div>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+
+      <div>{ getStepContent(activeStep) }</div>
+    </div>
   );
 }
 
@@ -33,7 +90,8 @@ const propsMap = {
 
 const mapStateToProps = () => ({
   initialValues: {
-
+    cardDownloadUrl: 'https://www.mtgjson.com/api/v5/AllPrintings.json.xz',
+    tokenDownloadUrl: 'https://raw.githubusercontent.com/Cockatrice/Magic-Token/master/tokens.xml'
   }
 });
 
