adee2da49d1dc726f651d97d381be02f506cb907
Jeremy Letto <jeremy.letto@datasite.com>
2021-07-26 10:05:13 -0500

    capture any whitespace in regex

--- webclient/src/components/Message/Message.tsx
+++ webclient/src/components/Message/Message.tsx
@@ -10,8 +10,8 @@ import './Message.css';
 
 // eslint-disable-next-line
 const urlRegex = /(https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*))/g;
-const nameRegex = /(^[^: ]+):/;
-const mentionRegex = /(@[^ ]+)/g;
+const nameRegex = /(^[^:\s]+):/;
+const mentionRegex = /(@[^\s]+)/g;
 const cardCalloutsRegex = /(\[\[[^\]]+\]\])/g;
 const calloutsRegex = /(\[\[|\]\])/g;
 
