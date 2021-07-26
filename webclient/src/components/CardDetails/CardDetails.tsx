eeec08111a66ff299bc95693e7c3966088763d02
Jeremy Letto <jeremy.letto@datasite.com>
2021-07-26 01:39:30 -0500

    cleanup

--- webclient/src/components/CardDetails/CardDetails.tsx
+++ webclient/src/components/CardDetails/CardDetails.tsx
@@ -18,103 +18,109 @@ const CardDetails = ({ card }: CardProps) => {
         <Card card={card} />
       </div>
 
-      <div className='cardDetails-attributes'>
-        <div className='cardDetails-attribute'>
-          <span className='cardDetails-attribute__label'>Name:</span>
-          <span className='cardDetails-attribute__value'>{card?.name}</span>
-        </div>
+      {
+        card && (
+          <div>
+            <div className='cardDetails-attributes'>
+              <div className='cardDetails-attribute'>
+                <span className='cardDetails-attribute__label'>Name:</span>
+                <span className='cardDetails-attribute__value'>{card.name}</span>
+              </div>
 
-        {
-          (!card?.power && !card?.toughness) ? null : (
-            <div className='cardDetails-attribute'>
-              <span className='cardDetails-attribute__label'>P/T:</span>
-              <span className='cardDetails-attribute__value'>{card?.power || 0}/{card?.toughness || 0}</span>
+              {
+                (!card.power && !card.toughness) ? null : (
+                  <div className='cardDetails-attribute'>
+                    <span className='cardDetails-attribute__label'>P/T:</span>
+                    <span className='cardDetails-attribute__value'>{card.power || 0}/{card.toughness || 0}</span>
+                  </div>
+                )
+              }
+
+              {
+                !card.manaCost ? null : (
+                  <div className='cardDetails-attribute'>
+                    <span className='cardDetails-attribute__label'>Cost:</span>
+                    <span className='cardDetails-attribute__value'>{card.manaCost.replace(/\{|\}/g, '')}</span>
+                  </div>
+                )
+              }
+
+              {
+                !card.convertedManaCost ? null : (
+                  <div className='cardDetails-attribute'>
+                    <span className='cardDetails-attribute__label'>CMC:</span>
+                    <span className='cardDetails-attribute__value'>{card.convertedManaCost}</span>
+                  </div>
+                )
+              }
+
+              {
+                !card.colorIdentity?.length ? null : (
+                  <div className='cardDetails-attribute'>
+                    <span className='cardDetails-attribute__label'>Identity:</span>
+                    <span className='cardDetails-attribute__value'>{card.colorIdentity.join('')}</span>
+                  </div>
+                )
+              }
+
+              {
+                !card.colors?.length ? null : (
+                  <div className='cardDetails-attribute'>
+                    <span className='cardDetails-attribute__label'>Color(s):</span>
+                    <span className='cardDetails-attribute__value'>{card.colors.join('')}</span>
+                  </div>
+                )
+              }
+
+              {
+                !card.types?.length ? null : (
+                  <div className='cardDetails-attribute'>
+                    <span className='cardDetails-attribute__label'>Main Type:</span>
+                    <span className='cardDetails-attribute__value'>{card.types.join(', ')}</span>
+                  </div>
+                )
+              }
+
+              {
+                !card.type ? null : (
+                  <div className='cardDetails-attribute'>
+                    <span className='cardDetails-attribute__label'>Type:</span>
+                    <span className='cardDetails-attribute__value'>{card.type}</span>
+                  </div>
+                )
+              }
+
+              {
+                !card.side ? null : (
+                  <div className='cardDetails-attribute'>
+                    <span className='cardDetails-attribute__label'>Side:</span>
+                    <span className='cardDetails-attribute__value'>{card.side}</span>
+                  </div>
+                )
+              }
+
+              {
+                !card.layout ? null : (
+                  <div className='cardDetails-attribute'>
+                    <span className='cardDetails-attribute__label'>Layout:</span>
+                    <span className='cardDetails-attribute__value'>{card.layout}</span>
+                  </div>
+                )
+              }
             </div>
-          )
-        }
 
-        {
-          !card?.manaCost ? null : (
-            <div className='cardDetails-attribute'>
-              <span className='cardDetails-attribute__label'>Cost:</span>
-              <span className='cardDetails-attribute__value'>{card.manaCost.replace(/\{|\}/g, '')}</span>
+            <div className='cardDetails-text'>
+              <div className='cardDetails-text__current'>
+                {card.text?.trim()}
+              </div>
+
+              <div className='cardDetails-text__flavor'>
+                {card.flavorText?.trim()}
+              </div>
             </div>
-          )
-        }
-
-        {
-          !card?.convertedManaCost ? null : (
-            <div className='cardDetails-attribute'>
-              <span className='cardDetails-attribute__label'>CMC:</span>
-              <span className='cardDetails-attribute__value'>{card.convertedManaCost}</span>
-            </div>
-          )
-        }
-
-        {
-          !card?.colorIdentity?.length ? null : (
-            <div className='cardDetails-attribute'>
-              <span className='cardDetails-attribute__label'>Identity:</span>
-              <span className='cardDetails-attribute__value'>{card.colorIdentity.join('')}</span>
-            </div>
-          )
-        }
-
-        {
-          !card?.colors?.length ? null : (
-            <div className='cardDetails-attribute'>
-              <span className='cardDetails-attribute__label'>Color(s):</span>
-              <span className='cardDetails-attribute__value'>{card.colors.join('')}</span>
-            </div>
-          )
-        }
-
-        {
-          !card?.types?.length ? null : (
-            <div className='cardDetails-attribute'>
-              <span className='cardDetails-attribute__label'>Main Type:</span>
-              <span className='cardDetails-attribute__value'>{card.types.join(', ')}</span>
-            </div>
-          )
-        }
-
-        {
-          !card?.type ? null : (
-            <div className='cardDetails-attribute'>
-              <span className='cardDetails-attribute__label'>Type:</span>
-              <span className='cardDetails-attribute__value'>{card.type}</span>
-            </div>
-          )
-        }
-
-        {
-          !card?.side ? null : (
-            <div className='cardDetails-attribute'>
-              <span className='cardDetails-attribute__label'>Side:</span>
-              <span className='cardDetails-attribute__value'>{card.side}</span>
-            </div>
-          )
-        }
-
-        {
-          !card?.layout ? null : (
-            <div className='cardDetails-attribute'>
-              <span className='cardDetails-attribute__label'>Layout:</span>
-              <span className='cardDetails-attribute__value'>{card.layout}</span>
-            </div>
-          )
-        }
-      </div>
-
-      <div className='cardDetails-text'>
-        <div className='cardDetails-text__current'>
-          {card?.text?.trim()}
-        </div>
-
-        <div className='cardDetails-text__flavor'>
-          {card?.flavorText?.trim()}
-        </div>
-      </div>
+          </div>
+        )
+      }
     </div>
   );
 }
