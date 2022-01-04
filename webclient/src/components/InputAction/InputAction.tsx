7b48768913f9a2b08153ce0056869d246f2fef8d
Brent M Clark <brentmclark@gmail.com>
2022-01-03 22:18:22 -0600

    no rest spread on single primative when sibling components exist

--- webclient/src/components/InputAction/InputAction.tsx
+++ webclient/src/components/InputAction/InputAction.tsx
@@ -6,13 +6,13 @@ import { InputField } from 'components';
 
 import './InputAction.css';
 
-const InputAction = ({ action, label, name, validate, ...rest }) => (
+const InputAction = ({ action, label, name, validate, disabled }) => (
   <div className="input-action">
     <div className="input-action__item">
       <Field label={label} name={name} component={InputField} validate={validate} />
     </div>
     <div className="input-action__submit">
-      <Button {...rest} color="primary" variant="contained" type="submit">
+      <Button color="primary" variant="contained" type="submit" disabled={disabled}>
         {action}
       </Button>
     </div>
