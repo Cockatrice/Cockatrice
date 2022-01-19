d6118ead620054e375a56bb4098885f4c4046e33
ebbit1q <ebbit1q@gmail.com>
2022-01-19 18:13:44 +0100

    add version guards

--- common/debug_pb_message.cpp
+++ common/debug_pb_message.cpp
@@ -8,6 +8,9 @@
 #include <google/protobuf/message.h>
 #include <google/protobuf/text_format.h>
 
+// FastFieldValuePrinter is added in protobuf 3.4, going out of our way to add the old FieldValuePrinter is not worth it
+#if GOOGLE_PROTOBUF_VERSION > 3004000
+
 // value printer to use for all values, will snip too long contents
 class LimitedPrinter : public ::google::protobuf::TextFormat::FastFieldValuePrinter
 {
@@ -32,8 +35,10 @@ void LimitedPrinter::PrintString(const std::string &val,
 {
     auto length = val.length();
     if (length > MAX_TEXT_LENGTH) {
-        generator->PrintString("\"" + ::google::protobuf::CEscape(val.substr(0, MAX_NAME_LENGTH)) + "... ---snip--- (" +
-                               ::google::protobuf::StrCat(length) + " bytes total) \"");
+        std::string escaped;
+        ::google::protobuf::CEscapeAndAppend(::google::protobuf::CEscape(val.substr(0, MAX_NAME_LENGTH)), &escaped);
+        generator->PrintString("\"" + escaped + "... ---snip--- (" + ::google::protobuf::StrCat(length) +
+                               " bytes total) \"");
     } else {
         ::google::protobuf::TextFormat::FastFieldValuePrinter::PrintString(val, generator);
     }
@@ -74,6 +79,7 @@ void SafePrinter::applySafePrinter(const ::google::protobuf::Message &message,
         }
     }
 }
+#endif // GOOGLE_PROTOBUF_VERSION > 3004000
 
 QString getSafeDebugString(const ::google::protobuf::Message &message)
 {
@@ -86,10 +92,16 @@ QString getSafeDebugString(const ::google::protobuf::Message &message)
     ::google::protobuf::TextFormat::Printer printer;
     printer.SetSingleLineMode(true); // compact mode
     printer.SetExpandAny(true);      // prints all fields
+
+#if GOOGLE_PROTOBUF_VERSION > 3004000
     // printer takes ownership of the LimitedPrinter and will delete it
     printer.SetDefaultFieldValuePrinter(new LimitedPrinter());
     // check field names an create SafePrinters for necessary fields
     SafePrinter::applySafePrinter(message, printer);
+#else
+    // removing passwords from debug output will only be supported on newer protobuf versions
+    printer.SetTruncateStringFieldLongerThan(MAX_TEXT_LENGTH);
+#endif // GOOGLE_PROTOBUF_VERSION > 3004000
 
     std::string debug_string;
     printer.PrintToString(message, &debug_string);
