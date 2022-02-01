ccf77884ba5b38f8cf7557ee79c60338b13c1102
Brent Clark <brent@backboneiq.com>
2022-01-31 23:49:46 -0600

    add slide transition

--- webclient/src/components/Toast/Toast.tsx
+++ webclient/src/components/Toast/Toast.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
-import Snackbar from '@material-ui/core/Snackbar';
-import Alert, { AlertProps } from '@material-ui/lab/Alert';
 import ReactDOM from 'react-dom'
+
+import Alert, { AlertProps } from '@material-ui/lab/Alert';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import Slide, { SlideProps } from '@material-ui/core/Slide';
+import Snackbar from '@material-ui/core/Snackbar';
+
 
 const iconMapping = {
   success: <CheckCircleIcon />
@@ -24,11 +27,17 @@ function Toast(props) {
     if (reason === 'clickaway') {
       return;
     }
-
     onClose(event);
   };
+
   const node = (
-    <Snackbar open={open} autoHideDuration={autoHideDuration} onClose={handleClose}>
+    <Snackbar
+      open={open}
+      autoHideDuration={autoHideDuration}
+      onClose={handleClose}
+      TransitionComponent={TransitionLeft}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+    >
       <Alert onClose={handleClose} severity={severity} iconMapping={iconMapping}>
         {children}
       </Alert>
@@ -46,7 +55,13 @@ function Toast(props) {
 
 Toast.defaultProps = {
   severity: 'success',
-  autoHideDuration: 6000,
+  // 10s wait before automatically dismissing the Toast.
+  autoHideDuration: 10000,
 }
+
+function TransitionLeft(props) {
+  return <Slide {...props} direction="left" />;
+}
+
 
 export default Toast
