f487420a7c0676cb59ce534fedb2822cc4c632c7
Jeremy Letto <jeremy.letto@datasite.com>
2022-02-05 19:44:20 -0600

    remove custom fallback

--- webclient/src/i18n-backend.ts
+++ webclient/src/i18n-backend.ts
@@ -6,9 +6,7 @@ class I18nBackend {
   static type: ModuleType = 'backend';
 
   read(language, namespace, callback) {
-    language = Language[language] || Language.en;
-
-    fetch(`/locales/${language}/${namespace}.json`)
+    fetch(`/locales/${Language[language]}/${namespace}.json`)
       .then(resp => resp.json().then(json => callback(null, json)))
       .catch(error => callback(error, null));
   }
