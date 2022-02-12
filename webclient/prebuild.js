1d9b0bd98cc8de0c8dd9debe57292334dcc2b9d7
Jeremy Letto <jeremy.letto@datasite.com>
2022-02-12 11:58:45 -0600

    automate master proto file

--- webclient/prebuild.js
+++ webclient/prebuild.js
@@ -2,41 +2,57 @@ const fse = require('fs-extra');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
+const protoFilesDir = './public/pb';
+const serverPropsFile = './src/server-props.json';
+const masterProtoFile = './src/proto-files.json';
+
 const sharedFiles = [
-  ['../common/pb', './public/pb'],
+  ['../common/pb', protoFilesDir],
   ['../cockatrice/resources/countries', './src/images/countries'],
 ];
 
-const serverPropsFile = './src/server-props.json';
 
-copySharedFiles();
-createServerProps();
+(async () => {
+  // make sure these files finish copying before master file is created
+  await copySharedFiles();
 
-function copySharedFiles() {
-  runCommand(() => {
-    sharedFiles.forEach(([src, dest]) => {
-      fse.copy(src, dest, { overwrite: true });
-    });
-  });
-}
+  createMasterProtoFile();
+  createServerProps();
+})();
 
-function createServerProps() {
-  runCommand(async () => {
-    fse.outputFile(serverPropsFile, JSON.stringify({
-      REACT_APP_VERSION: await getCommitHash(),
-    }));
-  });
-}
-
-async function getCommitHash() {
-  return (await exec('git rev-parse HEAD')).stdout.trim();
-}
-
-function runCommand(command) {
+async function copySharedFiles() {
   try {
-    command();
+    return await Promise.all(sharedFiles.map(([src, dest]) => fse.copy(src, dest, { overwrite: true })));
   } catch (e) {
     console.error(e);
     process.exitCode = 1;
   }
+}
+
+function createMasterProtoFile() {
+  try {
+    fse.readdir(protoFilesDir, (err, files) => {
+      if (err) throw err;
+
+      fse.outputFile(masterProtoFile, JSON.stringify(files.filter(file => /\.proto$/.test(file))));
+    });
+  } catch (e) {
+    console.error(e);
+    process.exitCode = 1;
+  }
+}
+
+async function createServerProps() {
+  try {
+    fse.outputFile(serverPropsFile, JSON.stringify({
+      REACT_APP_VERSION: await getCommitHash(),
+    }));
+  } catch (e) {
+    console.error(e);
+    process.exitCode = 1;
+  }
+}
+
+async function getCommitHash() {
+  return (await exec('git rev-parse HEAD')).stdout.trim();
 }
