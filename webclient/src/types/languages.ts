b21ee5ce63321409dc770d0723c8504666a4c1d5
Jeremy Letto <jeremy.letto@datasite.com>
2022-03-06 19:22:26 -0600

    add pt-BR translation

--- webclient/src/types/languages.ts
+++ webclient/src/types/languages.ts
@@ -2,16 +2,19 @@ export enum Language {
   'en-US' = 'en-US',
   'fr' = 'fr',
   'nl' = 'nl',
+  'pt-BR' = 'pt-BR',
 }
 
 export enum LanguageCountry {
   'en-US' = 'us',
   'fr' = 'fr',
   'nl' = 'nl',
+  'pt-BR' = 'pt'
 }
 
 export enum LanguageNative {
   'en-US' = 'English - US',
   'fr' = 'Français',
   'nl' = 'Nederlands',
+  'pt-BR' = 'Portugues do Brasil',
 }
