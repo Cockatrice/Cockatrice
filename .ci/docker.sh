79501a4af73c9a221851f9d0d359f198de146967
ebbit1q <ebbit1q@gmail.com>
2022-04-19 01:04:49 +0200

    fix the uid and gid of the user in the docker container (#4610)
    * fix the uid and gid of the user in the container
    
    this fixes this error:
    unsafe repository ('/src' is owned by someone else)
    this caused the hash to go missing in the version number
    
    * add --interactive option to .ci/docker.sh
    
    * add --dir to .ci/compile.sh
    
    * fix up the comments on the ci scripts
    
    * add extra comment to docker.sh

--- .ci/docker.sh
+++ .ci/docker.sh
@@ -3,11 +3,14 @@
 # This script is to be used by the ci environment from the project root directory, do not use it from somewhere else.
 
 # Creates or loads docker images to use in compilation, creates RUN function to start compilation on the docker image.
+# <arg> sets the name of the docker image, these correspond to directories in .ci
 # --get loads the image from a previously saved image cache, will build if no image is found
 # --build builds the image from the Dockerfile in .ci/$NAME
 # --save stores the image, if an image was loaded it will not be stored
+# --interactive immediately starts the image interactively for debugging
+# --set-cache <location> sets the location to cache the image or for ccache
 # requires: docker
-# uses env: NAME CACHE BUILD GET SAVE (correspond to args: <name> --set-cache <cache> --build --get --save)
+# uses env: NAME CACHE BUILD GET SAVE INTERACTIVE (correspond to args: <name> --set-cache <cache> --build --get --save --interactive)
 # sets env: RUN CCACHE_DIR IMAGE_NAME RUN_ARGS RUN_OPTS BUILD_SCRIPT
 # exitcode: 1 for failure, 2 for missing dockerfile, 3 for invalid arguments
 export BUILD_SCRIPT=".ci/compile.sh"
@@ -28,6 +31,10 @@ while [[ $# != 0 ]]; do
       GET=1
       shift
       ;;
+    '--interactive')
+      INTERACTIVE=1
+      shift
+      ;;
     '--save')
       SAVE=1
       shift
@@ -36,7 +43,7 @@ while [[ $# != 0 ]]; do
       CACHE=$2
       if ! [[ -d $CACHE ]]; then
         echo "could not find cache path: $CACHE" >&2
-        exit 3
+        return 3
       fi
       shift 2
       ;;
@@ -88,7 +95,6 @@ else
   fi
 fi
 
-
 # Get the docker image from previously stored save
 if [[ $GET ]]; then
   if [[ $img_save ]] && docker load --input "$img_save"; then
@@ -133,9 +139,12 @@ function RUN ()
 {
   echo "running image:"
   if docker images | grep "$IMAGE_NAME"; then
-    args=(--mount "type=bind,source=$PWD,target=/src" -w="/src")
+    local args=(--mount "type=bind,source=$PWD,target=/src")
+    args+=(--workdir "/src")
+    args+=(--user "$(id -u):$(id -g)")
     if [[ $CCACHE_DIR ]]; then
-      args+=(--mount "type=bind,source=$CCACHE_DIR,target=/.ccache" -e "CCACHE_DIR=/.ccache")
+      args+=(--mount "type=bind,source=$CCACHE_DIR,target=/.ccache")
+      args+=(--env "CCACHE_DIR=/.ccache")
     fi
     docker run "${args[@]}" $RUN_ARGS "$IMAGE_NAME" bash "$BUILD_SCRIPT" $RUN_OPTS "$@"
     return $?
@@ -144,3 +153,11 @@ function RUN ()
     return 3
   fi
 }
+
+# for debugging, start the docker image interactively instead of building
+# starts immediately, does not require sourcing or RUN
+if [[ $INTERACTIVE ]]; then
+  export BUILD_SCRIPT="-i"
+  export RUN_ARGS="$RUN_ARGS -it"
+  RUN
+fi
