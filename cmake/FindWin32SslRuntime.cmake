e41b8b6253eff871223a9989b1e1ff8767864bfa
ZeldaZach <zahalpern+github@gmail.com>
2022-05-04 15:09:31 -0400

    Try supporting OpenSSL via VCPKG, restoring baseline root paths
    Allow OpenSSL3

--- cmake/FindWin32SslRuntime.cmake
+++ cmake/FindWin32SslRuntime.cmake
@@ -1,22 +1,30 @@
 # Find the OpenSSL runtime libraries (.dll) for Windows that
 # will be needed by Qt in order to access https urls.
 if (WIN32)
-  if(NOT DEFINED ENV{CMAKE_GENERATOR_PLATFORM})
-    message(WARNING "CMAKE_GENERATOR_PLATFORM" env not defined, uncertain where to find OpenSSL)
-  elseif("$ENV{CMAKE_GENERATOR_PLATFORM}" STREQUAL "x64")
-    message(STATUS "Looking for OpenSSL for $ENV{CMAKE_GENERATOR_PLATFORM}")
+  if("${CMAKE_GENERATOR_PLATFORM}" STREQUAL "x64")
+    message(STATUS "Looking for OpenSSL for ${CMAKE_GENERATOR_PLATFORM}")
     set(_OPENSSL_ROOT_HINTS ${OPENSSL_ROOT_DIR} ENV OPENSSL_ROOT_DIR)
     file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _programfiles)
     set(_OPENSSL_ROOT_PATHS
-            "C:/OpenSSL-Win64/bin/"
+            "$ENV{VCPKG_PACKAGES_DIR}/x64-windows/bin"
+            "C:/OpenSSL-Win64/bin"
+            "C:/OpenSSL-Win64"
+            "C:/Tools/vcpkg/installed/x64-windows/bin"
+            "${_programfiles}/OpenSSL-Win64"
             )
     unset(_programfiles)
-  elseif("$ENV{CMAKE_GENERATOR_PLATFORM}" STREQUAL "Win32")
-    message(STATUS "Looking for OpenSSL for $ENV{CMAKE_GENERATOR_PLATFORM}")
+  elseif("${CMAKE_GENERATOR_PLATFORM}" STREQUAL "Win32")
+    message(STATUS "Looking for OpenSSL for ${CMAKE_GENERATOR_PLATFORM}")
     set(_OPENSSL_ROOT_HINTS ${OPENSSL_ROOT_DIR} ENV OPENSSL_ROOT_DIR)
     file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _programfiles)
     set(_OPENSSL_ROOT_PATHS
-            "C:/OpenSSL-Win32/bin/"
+            "$ENV{VCPKG_PACKAGES_DIR}/x86-windows/bin"
+            "C:/OpenSSL-Win32/bin"
+            "C:/OpenSSL-Win32"
+            "C:/OpenSSL"
+            "C:/Tools/vcpkg/installed/x86-windows/bin"
+            "${_programfiles}/OpenSSL"
+            "${_programfiles}/OpenSSL-Win32"
             )
     unset(_programfiles)
   else()
@@ -26,13 +34,13 @@ else ()
   set(_OPENSSL_ROOT_HINTS ${OPENSSL_ROOT_DIR} ENV OPENSSL_ROOT_DIR)
 endif()
 
-message(STATUS "Looking for OpenSSL @ $ENV{CMAKE_GENERATOR_PLATFORM} in ${_OPENSSL_ROOT_PATHS}")
-if("$ENV{CMAKE_GENERATOR_PLATFORM}" STREQUAL "x64")
-  FIND_FILE(WIN32SSLRUNTIME_LIBEAY NAMES libcrypto-1_1-x64.dll libcrypto.dll PATHS "${_OPENSSL_ROOT_PATHS}" NO_DEFAULT_PATH)
-  FIND_FILE(WIN32SSLRUNTIME_SSLEAY NAMES libssl-1_1-x64.dll libssl.dll PATHS "${_OPENSSL_ROOT_PATHS}" NO_DEFAULT_PATH)
-elseif("$ENV{CMAKE_GENERATOR_PLATFORM}" STREQUAL "Win32")
-  FIND_FILE(WIN32SSLRUNTIME_LIBEAY NAMES libcrypto-1_1.dll libcrypto.dll PATHS "${_OPENSSL_ROOT_PATHS}" NO_DEFAULT_PATH)
-  FIND_FILE(WIN32SSLRUNTIME_SSLEAY NAMES libssl-1_1.dll libssl.dll PATHS "${_OPENSSL_ROOT_PATHS}" NO_DEFAULT_PATH)
+message(STATUS "Looking for OpenSSL @ ${CMAKE_GENERATOR_PLATFORM} in ${_OPENSSL_ROOT_PATHS}")
+if("${CMAKE_GENERATOR_PLATFORM}" STREQUAL "x64")
+  FIND_FILE(WIN32SSLRUNTIME_LIBEAY NAMES libcrypto-1_1-x64.dll libcrypto-3-x64.dll libcrypto.dll PATHS ${_OPENSSL_ROOT_PATHS} NO_DEFAULT_PATH)
+  FIND_FILE(WIN32SSLRUNTIME_SSLEAY NAMES libssl-1_1-x64.dll libssl-3-x64.dll libssl.dll PATHS ${_OPENSSL_ROOT_PATHS} NO_DEFAULT_PATH)
+elseif("${CMAKE_GENERATOR_PLATFORM}" STREQUAL "Win32")
+  FIND_FILE(WIN32SSLRUNTIME_LIBEAY NAMES libcrypto-1_1.dll libcrypto-3.dll libcrypto.dll PATHS ${_OPENSSL_ROOT_PATHS} NO_DEFAULT_PATH)
+  FIND_FILE(WIN32SSLRUNTIME_SSLEAY NAMES libssl-1_1.dll libssl-3.dll libssl.dll PATHS ${_OPENSSL_ROOT_PATHS} NO_DEFAULT_PATH)
 endif()
 
 IF(WIN32SSLRUNTIME_LIBEAY AND WIN32SSLRUNTIME_SSLEAY)
