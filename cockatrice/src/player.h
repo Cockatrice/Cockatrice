873edf2b37d88dcbd24cab513a05f3ef198e2dea
CajunAvenger <kajunkittyavenger@gmail.com>
2022-07-18 10:27:07 -0500

    run format.sh again

--- cockatrice/src/player.h
+++ cockatrice/src/player.h
@@ -285,7 +285,8 @@ private:
                            bool allCards);
     void addRelatedCardActions(const CardItem *card, QMenu *cardMenu);
     void addRelatedCardView(const CardItem *card, QMenu *cardMenu);
-    void createCard(const CardItem *sourceCard, const QString &dbCardName, bool attach = false, bool persistent = false);
+    void
+    createCard(const CardItem *sourceCard, const QString &dbCardName, bool attach = false, bool persistent = false);
     void createAttachedCard(const CardItem *sourceCard, const QString &dbCardName, bool persistent = false);
     bool createRelatedFromRelation(const CardItem *sourceCard, const CardRelation *cardRelation);
 
