03cb21d9411e244ec56952f8acf00defbcae59f6
Jay Letto <jletto87@gmail.com>
2022-10-30 14:49:31 -0500

    add rel attr to links

--- webclient/src/websocket/utils/sanitizeHtml.util.ts
+++ webclient/src/websocket/utils/sanitizeHtml.util.ts
@@ -4,11 +4,14 @@ export function sanitizeHtml(msg: string): string {
   return sanitize(msg, {
     allowedTags: ['br', 'a', 'img', 'center', 'b', 'font'],
     allowedAttributes: {
-      '*': ['href', 'color', 'target'],
+      '*': ['href', 'color', 'rel', 'target'],
     },
     allowedSchemes: ['http', 'https', 'ftp'],
     transformTags: {
-      'a': sanitize.simpleTransform('a', { target: '_blank' }),
+      'a': sanitize.simpleTransform('a', {
+        target: '_blank',
+        rel: 'noopener noreferrer',
+      }),
     }
   });
 }
