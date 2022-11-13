23db1c3ce4514ef6d92cbd00d50edcb8088a32f6
Brent Clark <brent@backboneiq.com>
2022-11-12 21:20:34 -0600

    convert header to left nav

--- webclient/src/containers/Layout/Layout.tsx
+++ webclient/src/containers/Layout/Layout.tsx
@@ -1,12 +1,26 @@
 
-import { Header } from 'components';
+import LeftNav from './LeftNav';
+
+import './Layout.css'
 
 function Layout(props:LayoutProps) {
     const { children, className, showNav = true } = props;
     return (
-        <div className={className}>
-            <Header showNav={showNav} />
-            {children}
+        <div className="layout">
+            {showNav && <LeftNav />}
+            <section className="page">
+                <div className="page__body">
+                    {children}
+                </div>
+                {showNav && <BottomBar />}
+            </section>
+        </div>
+    )
+}
+
+function BottomBar(props) {
+    return (
+        <div className="bottom-bar__container">
         </div>
     )
 }
