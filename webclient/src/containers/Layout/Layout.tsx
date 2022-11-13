f06bc64a622486706810ecaa8259bef3ec74beda
Brent Clark <brent@backboneiq.com>
2022-11-12 21:49:14 -0600

    lint fix

--- webclient/src/containers/Layout/Layout.tsx
+++ webclient/src/containers/Layout/Layout.tsx
@@ -4,25 +4,25 @@ import LeftNav from './LeftNav';
 import './Layout.css'
 
 function Layout(props:LayoutProps) {
-    const { children, className, showNav = true } = props;
-    return (
-        <div className="layout">
-            {showNav && <LeftNav />}
-            <section className="page">
-                <div className="page__body">
-                    {children}
-                </div>
-                {showNav && <BottomBar />}
-            </section>
+  const { children, className, showNav = true } = props;
+  return (
+    <div className="layout">
+      {showNav && <LeftNav />}
+      <section className="page">
+        <div className="page__body">
+          {children}
         </div>
-    )
+        {showNav && <BottomBar />}
+      </section>
+    </div>
+  )
 }
 
 function BottomBar(props) {
-    return (
-        <div className="bottom-bar__container">
-        </div>
-    )
+  return (
+    <div className="bottom-bar__container">
+    </div>
+  )
 }
 
 interface LayoutProps {
