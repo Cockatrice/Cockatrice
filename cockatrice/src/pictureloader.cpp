82b29d406123a2188f1bb95babbe6b6603f04220
peter <peterjschroeder@gmail.com>
2022-12-18 13:18:27 -0500

    Some image sources require that a proper user agent is sent.

--- cockatrice/src/pictureloader.cpp
+++ cockatrice/src/pictureloader.cpp
@@ -386,6 +386,7 @@ void PictureLoaderWorker::startNextPicDownload()
     } else {
         QUrl url(picUrl);
         QNetworkRequest req(url);
+	req.setRawHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
         qDebug() << "PictureLoader: [card: " << cardBeingDownloaded.getCard()->getCorrectedName()
                  << " set: " << cardBeingDownloaded.getSetName()
                  << "]: Trying to download picture from url:" << url.toDisplayString();
