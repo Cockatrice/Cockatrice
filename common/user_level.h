8c539351e3f51b0e44b31b30dcac90ba3e1c6ffe
Zach H <zahalpern+github@gmail.com>
2023-08-06 22:45:45 -0400

    Fix Mac Builds (#4844)

--- common/user_level.h
+++ common/user_level.h
@@ -1,7 +1,7 @@
 #ifndef USER_LEVEL_H
 #define USER_LEVEL_H
 
-#ifdef Q_OS_OSX
+#ifdef Q_OS_MACOS
 // avoid collision from Mac OS X's ConditionalMacros.h
 // https://github.com/protocolbuffers/protobuf/issues/119
 #undef TYPE_BOOL
