4afff421d2ba72c11404b1745c5fb62d88b3f441
tooomm <tooomm@users.noreply.github.com>
2024-01-23 23:55:29 +0100

    remove specific CL_MPcount

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -163,12 +163,7 @@ cmake .. "${flags[@]}"
 echo "::endgroup::"
 
 echo "::group::Build project"
-if [[ $PARALLEL_COUNT && $RUNNER_OS == Windows ]]; then
-  # --parallel option doesn't set /MP, see https://gitlab.kitware.com/cmake/cmake/-/issues/20564
-  cmake --build . "${buildflags[@]}" -- -p:CL_MPcount="$PARALLEL_COUNT"
-else
-  cmake --build . "${buildflags[@]}"
-fi
+cmake --build . "${buildflags[@]}"
 echo "::endgroup::"
 
 if [[ $USE_CCACHE ]]; then
