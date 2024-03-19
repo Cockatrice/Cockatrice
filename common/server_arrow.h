Cockatrice/Cockatrice

commit 6bf7c798913f2f127ce8e7dba9f0a5870f1c9f34

copy arrows on transform (#4907)

common/server_arrow.h
@@ -25,9 +25,17 @@ public:
     {
         return startCard;
     }
+    void setStartCard(Server_Card *startCard_)
+    {
+        startCard = startCard_;
+    }
     Server_ArrowTarget *getTargetItem() const
     {
         return targetItem;
+    }
+    void setTargetItem(Server_ArrowTarget *targetItem_)
+    {
+        targetItem = targetItem_;
     }
     const color &getColor() const
     {

common/server_player.cpp
@@ -1475,6 +1475,47 @@ Server_Player::cmdCreateToken(const Command_CreateToken &cmd, ResponseContainer 
                                      attachedCard->getZone()->getPlayer()->getPlayerId());
             }
 
+            // Copy Arrows
+            const QList<Server_Player *> &players = game->getPlayers().values();
+            for (auto player : players) {
+                QMapIterator<int, Server_Arrow *> arrowIterator(player->getArrows());
+                while (arrowIterator.hasNext()) {
+                    Server_Arrow *arrow = arrowIterator.next().value();
+                    bool sendGameEvent = false;
+                    const auto *startCard = arrow->getStartCard();
+                    if (startCard == targetCard) {
+                        sendGameEvent = true;
+                        arrow->setStartCard(card);
+                        startCard = card;
+                    }
+                    const auto *targetItem = arrow->getTargetItem();
+                    if (targetItem == targetCard) {
+                        sendGameEvent = true;
+                        arrow->setTargetItem(card);
+                        targetItem = card;
+                    }
+                    if (sendGameEvent) {
+                        Event_CreateArrow event;
+                        ServerInfo_Arrow *arrowInfo = event.mutable_arrow_info();
+                        arrowInfo->set_id(arrow->getId());
+                        arrowInfo->set_start_player_id(player->getPlayerId());
+                        arrowInfo->set_start_zone(startCard->getZone()->getName().toStdString());
+                        arrowInfo->set_start_card_id(startCard->getId());
+                        const Server_Player *arrowTargetPlayer = qobject_cast<const Server_Player *>(targetItem);
+                        if (arrowTargetPlayer != nullptr) {
+                            arrowInfo->set_target_player_id(arrowTargetPlayer->getPlayerId());
+                        } else {
+                            const Server_Card *arrowTargetCard = qobject_cast<const Server_Card *>(targetItem);
+                            arrowInfo->set_target_player_id(arrowTargetCard->getZone()->getPlayer()->getPlayerId());
+                            arrowInfo->set_target_zone(arrowTargetCard->getZone()->getName().toStdString());
+                            arrowInfo->set_target_card_id(arrowTargetCard->getId());
+                        }
+                        arrowInfo->mutable_arrow_color()->CopyFrom(arrow->getColor());
+                        ges.enqueueGameEvent(event, player->getPlayerId());
+                    }
+                }
+            }
+
             targetCard->resetState();
             card->setStashedCard(targetCard);
             break;

commit 789cd1ebcc99b5a42c06c24c3df450b551d59fe2

assign new arrow id when arrow is moved to transformed card
fixes bug introduced in #4907
fixes #5008

common/server_arrow.h
@@ -21,6 +21,10 @@ public:
     {
         return id;
     }
+    void setId(int _id)
+    {
+        id = _id;
+    }
     Server_Card *getStartCard() const
     {
         return startCard;

common/server_player.cpp
@@ -296,6 +296,12 @@ void Server_Player::addArrow(Server_Arrow *arrow)
     arrows.insert(arrow->getId(), arrow);
 }
 
+void Server_Player::updateArrowId(int id)
+{
+    auto *arrow = arrows.take(id);
+    arrows.insert(arrow->getId(), arrow);
+}
+
 bool Server_Player::deleteArrow(int arrowId)
 {
     Server_Arrow *arrow = arrows.value(arrowId, 0);
@@ -497,9 +503,7 @@ Response::ResponseCode Server_Player::moveCard(GameEventStorage &ges,
             const QList<Server_Player *> &players = game->getPlayers().values();
             for (auto player : players) {
                 QList<int> arrowsToDelete;
-                QMapIterator<int, Server_Arrow *> arrowIterator(player->getArrows());
-                while (arrowIterator.hasNext()) {
-                    Server_Arrow *arrow = arrowIterator.next().value();
+                for (Server_Arrow *arrow : player->getArrows()) {
                     if ((arrow->getStartCard() == card) || (arrow->getTargetItem() == card))
                         arrowsToDelete.append(arrow->getId());
                 }
@@ -1478,9 +1482,8 @@ Server_Player::cmdCreateToken(const Command_CreateToken &cmd, ResponseContainer 
             // Copy Arrows
             const QList<Server_Player *> &players = game->getPlayers().values();
             for (auto player : players) {
-                QMapIterator<int, Server_Arrow *> arrowIterator(player->getArrows());
-                while (arrowIterator.hasNext()) {
-                    Server_Arrow *arrow = arrowIterator.next().value();
+                QList<int> changedArrowIds;
+                for (Server_Arrow *arrow : player->getArrows()) {
                     bool sendGameEvent = false;
                     const auto *startCard = arrow->getStartCard();
                     if (startCard == targetCard) {
@@ -1497,7 +1500,10 @@ Server_Player::cmdCreateToken(const Command_CreateToken &cmd, ResponseContainer 
                     if (sendGameEvent) {
                         Event_CreateArrow _event;
                         ServerInfo_Arrow *arrowInfo = _event.mutable_arrow_info();
-                        arrowInfo->set_id(arrow->getId());
+                        changedArrowIds.append(arrow->getId());
+                        int id = player->newArrowId();
+                        arrow->setId(id);
+                        arrowInfo->set_id(id);
                         arrowInfo->set_start_player_id(player->getPlayerId());
                         arrowInfo->set_start_zone(startCard->getZone()->getName().toStdString());
                         arrowInfo->set_start_card_id(startCard->getId());
@@ -1513,6 +1519,9 @@ Server_Player::cmdCreateToken(const Command_CreateToken &cmd, ResponseContainer 
                         arrowInfo->mutable_arrow_color()->CopyFrom(arrow->getColor());
                         ges.enqueueGameEvent(_event, player->getPlayerId());
                     }
+                }
+                for (int id : changedArrowIds) {
+                    player->updateArrowId(id);
                 }
             }
 
@@ -1579,9 +1588,7 @@ Server_Player::cmdCreateArrow(const Command_CreateArrow &cmd, ResponseContainer 
         return Response::RespNameNotFound;
     }
 
-    QMapIterator<int, Server_Arrow *> arrowIterator(arrows);
-    while (arrowIterator.hasNext()) {
-        Server_Arrow *temp = arrowIterator.next().value();
+    for (Server_Arrow *temp : arrows) {
         if ((temp->getStartCard() == startCard) && (temp->getTargetItem() == targetItem)) {
             return Response::RespContextError;
         }

common/server_player.h
@@ -164,6 +164,7 @@ public:
 
     void addZone(Server_CardZone *zone);
     void addArrow(Server_Arrow *arrow);
+    void updateArrowId(int id);
     bool deleteArrow(int arrowId);
     void addCounter(Server_Counter *counter);
 