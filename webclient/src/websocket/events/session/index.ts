d128935a4a72d79721514158aa79503a6d0d2446
ZeldaZach <zahalpern+github@gmail.com>
2024-06-16 23:03:37 -0400

    Skeleton + RemoveMessages

--- webclient/src/websocket/events/session/index.ts
+++ webclient/src/websocket/events/session/index.ts
@@ -13,16 +13,19 @@ import { userLeft } from './userLeft';
 import { userMessage } from './userMessage';
 
 export const SessionEvents: ProtobufEvents = {
-  '.Event_AddToList.ext': addToList,
   '.Event_ConnectionClosed.ext': connectionClosed,
+  '.Event_GameJoined.ext': () => {},
   '.Event_ListRooms.ext': listRooms,
   '.Event_NotifyUser.ext': notifyUser,
   '.Event_PlayerPropertiesChanges.ext': playerPropertiesChanges,
   '.Event_RemoveFromList.ext': removeFromList,
+  '.Event_ReplayAdded.ext': () => {},
+  '.Event_ServerCompleteList.ext': () => {},
   '.Event_ServerIdentification.ext': serverIdentification,
   '.Event_ServerMessage.ext': serverMessage,
   '.Event_ServerShutdown.ext': serverShutdown,
   '.Event_UserJoined.ext': userJoined,
   '.Event_UserLeft.ext': userLeft,
   '.Event_UserMessage.ext': userMessage,
+  '.Event_AddToList.ext': addToList,
 }
