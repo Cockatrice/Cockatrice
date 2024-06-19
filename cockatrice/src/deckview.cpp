8bb655c2b7b8803fc8b4c5ca24fcd561cdbcb570
ZeldaZach <zahalpern+github@gmail.com>
2024-06-19 13:41:17 -0400

    Cleanup

--- cockatrice/src/deckview.cpp
+++ cockatrice/src/deckview.cpp
@@ -215,7 +215,7 @@ void DeckViewCardContainer::addCard(DeckViewCard *card)
     cardsByType.insert(card->getInfo() ? card->getInfo()->getMainCardType() : "", card);
 }
 
-void DeckViewCardContainer::removeCard(DeckViewCard *card) Ï
+void DeckViewCardContainer::removeCard(DeckViewCard *card)
 {
     cards.removeOne(card);
     cardsByType.remove(card->getInfo() ? card->getInfo()->getMainCardType() : "", card);
