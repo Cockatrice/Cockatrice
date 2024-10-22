eab26d14e90ec9fd23df08a2e0d479ead88ad805
RickyRister <ricky.rister.wang@gmail.com>
2024-10-21 18:22:51 -0700

    change default fast forward shortcut to Ctrl+F

--- cockatrice/src/settings/shortcuts_settings.h
+++ cockatrice/src/settings/shortcuts_settings.h
@@ -606,7 +606,7 @@ private:
                                            parseSequenceString("Space"),
                                            ShortcutGroup::Replays)},
         {"Replays/fastForwardButton", ShortcutKey(QT_TRANSLATE_NOOP("shortcutsTab", "Toggle Fast Forward"),
-                                                  parseSequenceString("."),
+                                                  parseSequenceString("Ctrl+F"),
                                                   ShortcutGroup::Replays)}};
 };
 
