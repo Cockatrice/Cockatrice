58f53676fe1c9223f0bc5555db24fc700c7afc41
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2024-11-03 09:33:13 +0100

    Change CardInfo's PixmapCacheKey to be the UUID of the preferred set after database loading has finished. Otherwise, and if no UUID of a preferred set is available, default to the card name.

--- cockatrice/src/client/ui/picture_loader.cpp
+++ cockatrice/src/client/ui/picture_loader.cpp
@@ -43,6 +43,14 @@ PictureToLoad::PictureToLoad(CardInfoPtr _card)
             sortedSets << CardSet::newInstance("", "", "", QDate());
         }
         std::sort(sortedSets.begin(), sortedSets.end(), SetDownloadPriorityComparator());
+        // If the pixmapCacheKey corresponds to a specific set, we have to try to load it first.
+        for (const auto &set : card->getSets()) {
+            if (QLatin1String("card_") + QString(set.getProperty("uuid")) == card->getPixmapCacheKey()) {
+                long long setIndex = sortedSets.indexOf(set.getPtr());
+                CardSetPtr set_for_card_uuid = sortedSets.takeAt(setIndex);
+                sortedSets.prepend(set_for_card_uuid);
+            }
+        }
         // The first time called, nextSet will also populate the Urls for the first set.
         nextSet();
     }
@@ -170,8 +178,10 @@ void PictureLoaderWorker::processLoadQueue()
         qDebug().nospace() << "PictureLoader: [card: " << cardName << " set: " << setName
                            << "]: Trying to load picture";
 
-        if (cardImageExistsOnDisk(setName, correctedCardName)) {
-            continue;
+        if (CardDatabaseManager::getInstance()->isUuidForPreferredPrinting(cardName, cardBeingLoaded.getCard()->getPixmapCacheKey())) {
+            if (cardImageExistsOnDisk(setName, correctedCardName)) {
+                continue;
+            }
         }
 
         qDebug().nospace() << "PictureLoader: [card: " << cardName << " set: " << setName
