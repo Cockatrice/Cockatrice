f718a86d5a22738af27dfbfceaa3816dd70ccf47
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2024-11-16 09:36:32 +0100

    Reformat code so the linter will stop yelling at me.

--- cockatrice/src/client/ui/widgets/cards/card_info_picture_with_text_overlay_widget.h
+++ cockatrice/src/client/ui/widgets/cards/card_info_picture_with_text_overlay_widget.h
@@ -2,20 +2,22 @@
 #define CARD_PICTURE_WITH_TEXT_OVERLAY_H
 
 #include "card_info_picture_widget.h"
+
 #include <QColor>
 #include <QSize>
 #include <QTextOption>
 
-class CardInfoPictureWithTextOverlayWidget : public CardInfoPictureWidget {
+class CardInfoPictureWithTextOverlayWidget : public CardInfoPictureWidget
+{
     Q_OBJECT
 
 public:
     explicit CardInfoPictureWithTextOverlayWidget(QWidget *parent = nullptr,
-                                        bool hoverToZoomEnabled = false,
-                                        const QColor &textColor = Qt::white,
-                                        const QColor &outlineColor = Qt::black,
-                                        int fontSize = 12,
-                                        Qt::Alignment alignment = Qt::AlignCenter);
+                                                  bool hoverToZoomEnabled = false,
+                                                  const QColor &textColor = Qt::white,
+                                                  const QColor &outlineColor = Qt::black,
+                                                  int fontSize = 12,
+                                                  Qt::Alignment alignment = Qt::AlignCenter);
 
     void setOverlayText(const QString &text);
     void setTextColor(const QColor &color);
