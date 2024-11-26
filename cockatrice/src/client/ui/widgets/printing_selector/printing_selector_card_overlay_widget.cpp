aa9dd24edae97719544dceaf6e3a8bfdf8ae31a5
Zach Halpern <zahalpern+github@gmail.com>
2024-11-25 21:37:10 -0500

    Fix Qt5 Builds

--- cockatrice/src/client/ui/widgets/printing_selector/printing_selector_card_overlay_widget.cpp
+++ cockatrice/src/client/ui/widgets/printing_selector/printing_selector_card_overlay_widget.cpp
@@ -68,9 +68,9 @@ void PrintingSelectorCardOverlayWidget::resizeEvent(QResizeEvent *event)
 }
 
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
-void PrintingSelectorCardOverlayWidget::enterEvent(QEnterEvent *event) override
+void PrintingSelectorCardOverlayWidget::enterEvent(QEnterEvent *event)
 #else
-void CardInfoPictPrintingSelectorCardOverlayWidget::enterEvent(QEvent *event) override
+void CardInfoPictPrintingSelectorCardOverlayWidget::enterEvent(QEvent *event)
 #endif
 {
     QWidget::enterEvent(event);
