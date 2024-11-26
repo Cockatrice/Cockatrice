669844167878dce508913288c8b47f3cb7200ffa
Zach Halpern <zahalpern+github@gmail.com>
2024-11-25 21:33:48 -0500

    Fix Qt5 Builds

--- cockatrice/src/client/ui/widgets/printing_selector/printing_selector_card_overlay_widget.h
+++ cockatrice/src/client/ui/widgets/printing_selector/printing_selector_card_overlay_widget.h
@@ -28,7 +28,11 @@ public:
 
 protected:
     void resizeEvent(QResizeEvent *event) override;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
     void enterEvent(QEnterEvent *event) override;
+#else
+    void enterEvent(QEvent *event) override;
+#endif
     void leaveEvent(QEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
     void customMenu(QPoint point);
