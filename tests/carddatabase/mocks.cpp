bde0b3bcfcb285ef3e7675687ca157fa8fc33ed4
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2024-12-06 03:25:36 +0100

    Fix tests.

--- tests/carddatabase/mocks.cpp
+++ tests/carddatabase/mocks.cpp
@@ -167,6 +167,9 @@ void SettingsCache::setOverrideAllCardArtWithPersonalPreference(QT_STATE_CHANGED
 void SettingsCache::setBumpSetsWithCardsInDeckToTop(QT_STATE_CHANGED_T /* _bumpSetsWithCardsInDeckToTop */)
 {
 }
+void setPrintingSelectorSortOrder(int /* _printingSelectorSortOrder */)
+{
+}
 void SettingsCache::setHorizontalHand(QT_STATE_CHANGED_T /* _horizontalHand */)
 {
 }
