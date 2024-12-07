77cdda62dc1b4e899e7a074d647f41d4e1f8c0af
RickyRister <ricky.rister.wang@gmail.com>
2024-12-07 02:50:34 -0800

    get thing to work

--- cockatrice/src/game/player/player.cpp
+++ cockatrice/src/game/player/player.cpp
@@ -1331,10 +1331,10 @@ void Player::actMoveTopCardsToExile()
 
 void Player::actMoveTopCardsUntil()
 {
-    moveTopCardTimer->stop();
-    movingCardsUntil = false;
+    stopMoveTopCardsUntil();
+
     QString expr = previousMovingCardsUntilExpr;
-    uint numberOfHits = 1;
+    int numberOfHits = previousMovingCardsUntilNumberOfHits;
     for (;;) {
         DlgMoveTopCardsUntil dlg(game, expr, numberOfHits);
         if (!dlg.exec()) {
@@ -1355,9 +1355,11 @@ void Player::actMoveTopCardsUntil()
         }
     }
     previousMovingCardsUntilExpr = expr;
+    previousMovingCardsUntilNumberOfHits = numberOfHits;
     if (zones.value("deck")->getCards().empty()) {
-        movingCardsUntil = false;
+        stopMoveTopCardsUntil();
     } else {
+        movingCardsUntilCounter = numberOfHits;
         movingCardsUntil = true;
         actMoveTopCardToPlay();
     }
@@ -1366,11 +1368,28 @@ void Player::actMoveTopCardsUntil()
 void Player::moveOneCardUntil(const CardInfoPtr card)
 {
     moveTopCardTimer->stop();
-    if (zones.value("deck")->getCards().empty() || card.isNull() || movingCardsUntilFilter.check(card)) {
-        movingCardsUntil = false;
+    if (zones.value("deck")->getCards().empty() || card.isNull()) {
+        stopMoveTopCardsUntil();
+    } else if (movingCardsUntilFilter.check(card)) {
+        --movingCardsUntilCounter;
+        if (movingCardsUntilCounter > 0) {
+            moveTopCardTimer->start();
+        } else {
+            stopMoveTopCardsUntil();
+        }
     } else {
         moveTopCardTimer->start();
     }
+}
+
+/**
+ * @brief Immediately stops any ongoing `play top card to stack until...` process, resetting all variables involved.
+ */
+void Player::stopMoveTopCardsUntil()
+{
+    moveTopCardTimer->stop();
+    movingCardsUntilCounter = 0;
+    movingCardsUntil = false;
 }
 
 void Player::actMoveTopCardToBottom()
