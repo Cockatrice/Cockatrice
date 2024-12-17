8980d8789ebcdc5522bfc2853a4f6480ef453d77
RickyRister <ricky.rister.wang@gmail.com>
2024-12-16 23:50:07 -0800

    add override specifiers
    it wouldn't compile otherwise

--- cockatrice/src/game/zones/view_zone_widget.h
+++ cockatrice/src/game/zones/view_zone_widget.h
@@ -66,8 +66,8 @@ private slots:
     void resizeToZoneContents();
     void handleScrollBarChange(int value);
     void zoneDeleted();
-    void moveEvent(QGraphicsSceneMoveEvent * /* event */);
-    void resizeEvent(QGraphicsSceneResizeEvent * /* event */);
+    void moveEvent(QGraphicsSceneMoveEvent * /* event */) override;
+    void resizeEvent(QGraphicsSceneResizeEvent * /* event */) override;
 
 public:
     ZoneViewWidget(Player *_player,
@@ -87,8 +87,8 @@ public:
     void retranslateUi();
 
 protected:
-    void closeEvent(QCloseEvent *event);
-    void initStyleOption(QStyleOption *option) const;
+    void closeEvent(QCloseEvent *event) override;
+    void initStyleOption(QStyleOption *option) const override;
 };
 
 #endif
