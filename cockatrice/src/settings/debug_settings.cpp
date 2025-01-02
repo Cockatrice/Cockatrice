1faccd900733d1137a3643fc7f175c435fce7503
RickyRister <ricky.rister.wang@gmail.com>
2025-01-01 19:48:37 -0800

    create debug.ini if not exists

--- cockatrice/src/settings/debug_settings.cpp
+++ cockatrice/src/settings/debug_settings.cpp
@@ -1,8 +1,14 @@
 #include "debug_settings.h"
 
+#include <QtCore/QFile>
+
 DebugSettings::DebugSettings(const QString &settingPath, QObject *parent)
     : SettingsManager(settingPath + "debug.ini", parent)
 {
+    // force debug.ini to be created if it doesn't exist yet
+    if (!QFile(settingPath + "debug.ini").exists()) {
+        setValue(false, "showCardId", "debug");
+    }
 }
 
 bool DebugSettings::getShowCardId()
