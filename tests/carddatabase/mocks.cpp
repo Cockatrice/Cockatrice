1e7b829c3c1d26ae16698df52b08f4c3ae4183e6
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-01-03 14:52:43 +0100

    Fix mocks.

--- tests/carddatabase/mocks.cpp
+++ tests/carddatabase/mocks.cpp
@@ -188,6 +188,9 @@ void SettingsCache::setPrintingSelectorCardSizeSliderVisible(QT_STATE_CHANGED_T 
 void SettingsCache::setPrintingSelectorNavigationButtonsVisible(QT_STATE_CHANGED_T /* _navigationButtonsVisible */)
 {
 }
+void SettingsCache::setVisualDeckStorageSortingOrder(int /* _visualDeckStorageSortingOrder */)
+{
+}
 void SettingsCache::setVisualDeckStorageCardSize(int /* _visualDeckStorageCardSize */)
 {
 }
