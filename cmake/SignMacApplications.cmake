2b3c47148e09d057a7c920f6dc20e37a4fe67ee8
Zach H <zahalpern+github@gmail.com>
2025-01-10 16:46:05 -0500

    GHA MacOS Only Sign/Notarize if self repo (#5437)

--- cmake/SignMacApplications.cmake
+++ cmake/SignMacApplications.cmake
@@ -1,6 +1,8 @@
 # This script re-signs all apps after CPack packages them. This is necessary because CPack modifies
 # the library references used by Cockatrice to App relative paths, invalidating the code signature.
-if(APPLE)
+string(LENGTH $ENV{MACOS_CERTIFICATE_NAME} MACOS_CERTIFICATE_NAME_LEN)
+
+if(APPLE AND MACOS_CERTIFICATE_NAME_LEN GREATER 0)
   set(APPLICATIONS "cockatrice" "servatrice" "oracle" "dbconverter")
   foreach(app_name IN LISTS APPLICATIONS)
     set(FULL_APP_PATH "${CPACK_TEMPORARY_INSTALL_DIRECTORY}/${app_name}.app")
