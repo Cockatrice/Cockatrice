a8b7fb91e4cfe11adb541e202e1af87caf53a245
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-01-19 17:01:39 +0100

    Commander details display widget.

---
+++ cockatrice/src/client/tabs/api/edhrec/edhrec_commander_api_response_commander_details_display_widget.h
@@ -0,0 +1,26 @@
+#ifndef EDHREC_COMMANDER_API_RESPONSE_COMMANDER_DETAILS_DISPLAY_WIDGET_H
+#define EDHREC_COMMANDER_API_RESPONSE_COMMANDER_DETAILS_DISPLAY_WIDGET_H
+
+#include "api_response/edhrec_commander_api_response_commander_details.h"
+
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QWidget>
+
+class EdhrecCommanderResponseCommanderDetailsDisplayWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit EdhrecCommanderResponseCommanderDetailsDisplayWidget(
+        QWidget *parent,
+        const EdhrecCommanderApiResponseCommanderDetails &_commanderDetails);
+    void retranslateUi();
+
+private:
+    QLabel *label;
+    QLabel *salt;
+    QVBoxLayout *layout;
+    EdhrecCommanderApiResponseCommanderDetails commanderDetails;
+};
+
+#endif // EDHREC_COMMANDER_API_RESPONSE_COMMANDER_DETAILS_DISPLAY_WIDGET_H
