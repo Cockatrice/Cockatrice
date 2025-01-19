e752578d1577e287bd940a302a2911a707ca507d
BruebachL <44814898+BruebachL@users.noreply.github.com>
2025-01-19 13:59:53 +0100

    Add a button to easily view the transformed version of a card. (#5498)
    * Add a button to easily view the transformed version of a card.
    
    * Minor reword
    
    * Minor fix
    
    ---------
    
    Co-authored-by: Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
    Co-authored-by: ZeldaZach <zahalpern+github@gmail.com>

--- cockatrice/src/client/ui/widgets/cards/card_info_frame_widget.cpp
+++ cockatrice/src/client/ui/widgets/cards/card_info_frame_widget.cpp
@@ -23,6 +23,11 @@ CardInfoFrameWidget::CardInfoFrameWidget(const QString &cardName, QWidget *paren
     text->setObjectName("text");
     connect(text, SIGNAL(linkActivated(const QString &)), this, SLOT(setCard(const QString &)));
 
+    viewTransformationButton = new QPushButton();
+    viewTransformationButton->setObjectName("viewTransformationButton");
+    connect(viewTransformationButton, &QPushButton::clicked, this, &CardInfoFrameWidget::viewTransformation);
+    viewTransformationButton->setVisible(false);
+
     tab1 = new QWidget(this);
     tab2 = new QWidget(this);
     tab3 = new QWidget(this);
@@ -69,6 +74,7 @@ void CardInfoFrameWidget::retranslateUi()
     setTabText(ImageOnlyView, tr("Image"));
     setTabText(TextOnlyView, tr("Description"));
     setTabText(ImageAndTextView, tr("Both"));
+    viewTransformationButton->setText(tr("View transformation"));
 }
 
 void CardInfoFrameWidget::setViewMode(int mode)
@@ -80,10 +86,12 @@ void CardInfoFrameWidget::setViewMode(int mode)
         case ImageOnlyView:
         case TextOnlyView:
             tab1Layout->addWidget(pic);
+            tab1Layout->addWidget(viewTransformationButton);
             tab2Layout->addWidget(text);
             break;
         case ImageAndTextView:
             splitter->addWidget(pic);
+            splitter->addWidget(viewTransformationButton);
             splitter->addWidget(text);
             break;
         default:
@@ -95,6 +103,7 @@ void CardInfoFrameWidget::setViewMode(int mode)
 
 void CardInfoFrameWidget::setCard(CardInfoPtr card)
 {
+    viewTransformationButton->setVisible(false);
     if (info) {
         disconnect(info.data(), nullptr, this, nullptr);
     }
@@ -103,6 +112,16 @@ void CardInfoFrameWidget::setCard(CardInfoPtr card)
 
     if (info) {
         connect(info.data(), SIGNAL(destroyed()), this, SLOT(clearCard()));
+    }
+
+    if (info) {
+        const auto &cardRelations = info->getAllRelatedCards();
+        for (const auto &cardRelation : cardRelations) {
+            if (cardRelation->getDoesTransform()) {
+                viewTransformationButton->setVisible(true);
+                break;
+            }
+        }
     }
 
     text->setCard(info);
@@ -123,6 +142,19 @@ void CardInfoFrameWidget::setCard(AbstractCardItem *card)
 {
     if (card) {
         setCard(card->getInfo());
+    }
+}
+
+void CardInfoFrameWidget::viewTransformation()
+{
+    if (info) {
+        const auto &cardRelations = info->getAllRelatedCards();
+        for (const auto &cardRelation : cardRelations) {
+            if (cardRelation->getDoesTransform()) {
+                setCard(cardRelation->getName());
+                break;
+            }
+        }
     }
 }
 
