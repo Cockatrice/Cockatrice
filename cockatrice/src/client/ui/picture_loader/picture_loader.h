efa76939ac90be45c0e82db7feed48b47a2f241b
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-01-24 12:47:31 +0100

    Lint.

--- cockatrice/src/client/ui/picture_loader/picture_loader.h
+++ cockatrice/src/client/ui/picture_loader/picture_loader.h
@@ -9,7 +9,7 @@
 inline Q_LOGGING_CATEGORY(PictureLoaderLog, "picture_loader");
 inline Q_LOGGING_CATEGORY(PictureLoaderCardBackCacheFailLog, "picture_loader.card_back_cache_fail");
 
-    class PictureLoader : public QObject
+class PictureLoader : public QObject
 {
     Q_OBJECT
 public:
