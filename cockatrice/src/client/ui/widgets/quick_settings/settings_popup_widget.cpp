b6418e88883af29d75146566f8874f3f8175e330
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-02-10 11:33:15 +0100

    Make popup not close when interacting with child widgets.

--- cockatrice/src/client/ui/widgets/quick_settings/settings_popup_widget.cpp
+++ cockatrice/src/client/ui/widgets/quick_settings/settings_popup_widget.cpp
@@ -1,5 +1,6 @@
 #include "settings_popup_widget.h"
 
+#include <QApplication>
 #include <QFocusEvent>
 
 SettingsPopupWidget::SettingsPopupWidget(QWidget *parent) : QWidget(parent, Qt::Popup | Qt::FramelessWindowHint)
@@ -14,7 +15,9 @@ void SettingsPopupWidget::addSettingsWidget(QWidget *toAdd) const
 
 void SettingsPopupWidget::focusOutEvent(QFocusEvent *event)
 {
-    close();
+    if (!this->isAncestorOf(QApplication::focusWidget())) {
+        close();
+    }
     QWidget::focusOutEvent(event);
 }
 
