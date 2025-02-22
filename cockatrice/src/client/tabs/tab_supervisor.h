d2822406531f2b6680a57e81ab1190405db29599
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-02-22 16:00:40 +0100

    Import.

--- cockatrice/src/client/tabs/tab_supervisor.h
+++ cockatrice/src/client/tabs/tab_supervisor.h
@@ -4,6 +4,7 @@
 #include "../../deck/deck_loader.h"
 #include "../../server/user/user_list_proxy.h"
 #include "api/edhrec/tab_edhrec.h"
+#include "tab_generic_deck_editor.h"
 #include "visual_deck_storage/tab_deck_storage_visual.h"
 
 #include <QAbstractButton>
