36db9eb6d11c6d83d32216244485ce0dd8dcfab5
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-02-24 15:13:56 +0100

    Better filtering and analytics.

--- cockatrice/src/client/ui/widgets/deck_analytics/mana_devotion_widget.h
+++ cockatrice/src/client/ui/widgets/deck_analytics/mana_devotion_widget.h
@@ -1,6 +1,7 @@
 #ifndef MANA_DEVOTION_WIDGET_H
 #define MANA_DEVOTION_WIDGET_H
 #include "../../../../deck/deck_list_model.h"
+#include "../general/display/banner_widget.h"
 
 #include <QHBoxLayout>
 #include <QWidget>
@@ -13,16 +14,21 @@ class ManaDevotionWidget : public QWidget
 
 public:
     explicit ManaDevotionWidget(QWidget *parent = nullptr, DeckListModel *deck_list_model = nullptr);
-    std::unordered_map<char, int> analyzeManaDevotion();
     void updateDisplay();
 
     std::unordered_map<char, int> countManaSymbols(const QString &manaString);
     void mergeManaCounts(std::unordered_map<char, int> &manaCounts1, const std::unordered_map<char, int> &manaCounts2);
 
+public slots:
+    void setDeckModel(DeckListModel *deckModel);
+    std::unordered_map<char, int> analyzeManaDevotion();
+
 private:
     DeckListModel *deck_list_model;
+    BannerWidget *bannerWidget;
     std::unordered_map<char, int> manaDevotionMap;
-    QHBoxLayout *layout;
+    QVBoxLayout *layout;
+    QHBoxLayout *barLayout;
 };
 
 #endif // MANA_DEVOTION_WIDGET_H
