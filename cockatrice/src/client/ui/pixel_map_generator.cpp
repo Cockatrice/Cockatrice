6f5d36941698a6adbca14b86e751ea63b2da0e57
RickyRister <42636155+RickyRister@users.noreply.github.com>
2025-02-25 15:36:48 -0800

    Add folder dropdown icons to VDS (#5632)
    * add svg
    
    * update pixmap cache
    
    * get icon to work
    
    * hide icon when not clickable
    
    * use consistent naming
    
    * use expandOnly because apparently that leads to higher image quality

--- cockatrice/src/client/ui/pixel_map_generator.cpp
+++ cockatrice/src/client/ui/pixel_map_generator.cpp
@@ -357,6 +357,21 @@ QPixmap LockPixmapGenerator::generatePixmap(int height)
 
 QMap<int, QPixmap> LockPixmapGenerator::pmCache;
 
+QPixmap DropdownIconPixmapGenerator::generatePixmap(int height, bool expanded)
+{
+    QString key = QString::number(expanded) + ":" + QString::number(height);
+    if (pmCache.contains(key))
+        return pmCache.value(key);
+
+    QString name = expanded ? "dropdown_expanded" : "dropdown_collapsed";
+    QPixmap pixmap = tryLoadImage("theme:icons/" + name, QSize(height, height), true);
+
+    pmCache.insert(key, pixmap);
+    return pixmap;
+}
+
+QMap<QString, QPixmap> DropdownIconPixmapGenerator::pmCache;
+
 QPixmap loadColorAdjustedPixmap(const QString &name)
 {
     if (qApp->palette().windowText().color().lightness() > 200) {
