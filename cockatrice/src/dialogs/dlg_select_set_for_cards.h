290ef9bf5a2086df812c2f81034f58dbbe9d1940
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-03-16 10:24:42 +0100

    Qt version check.

--- cockatrice/src/dialogs/dlg_select_set_for_cards.h
+++ cockatrice/src/dialogs/dlg_select_set_for_cards.h
@@ -77,7 +77,11 @@ public:
 
 public slots:
     void mousePressEvent(QMouseEvent *event) override;
+#if (QT_VERSION >= QT_VERSION_CHECK(6, 0, 0))
     void enterEvent(QEnterEvent *event) override;
+#else
+    void enterEvent(QEvent *event) override;
+#endif
     void leaveEvent(QEvent *event) override;
     void dragMoveEvent(QDragMoveEvent *event) override;
 
