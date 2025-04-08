2b50a2d9f93a019551078d94128256f32628c4c7
RickyRister <ricky.rister.wang@gmail.com>
2025-04-07 21:44:40 -0700

    Use QMessageBox::question for force start confirmation

--- cockatrice/src/game/deckview/deck_view_container.cpp
+++ cockatrice/src/game/deckview/deck_view_container.cpp
@@ -305,7 +305,7 @@ void DeckViewContainer::forceStart()
 {
     const auto msg = tr("Are you sure you want to force start?\nThis will kick all non-ready players from the game.");
     const auto res =
-        QMessageBox::warning(this, tr("Cockatrice"), msg, QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+        QMessageBox::question(this, tr("Cockatrice"), msg, QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
     if (res == QMessageBox::No) {
         return;
     }
