3b758962e442d70e0adc9d1a126e05cf019bb1ed
RickyRister <42636155+RickyRister@users.noreply.github.com>
2025-04-11 20:00:46 -0700

    Add search filter to card view window (#5791)
    * refactor out search syntax help window
    
    * add search bar to ZoneViewWidget
    
    * implement filter logic

---
+++ cockatrice/src/game/filters/syntax_help.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_SYNTAX_HELP_H
+#define SEARCH_SYNTAX_HELP_H
+
+#include <QLineEdit>
+#include <QTextBrowser>
+
+QTextBrowser *createSearchSyntaxHelpWindow(QLineEdit *lineEdit);
+
+#endif // SEARCH_SYNTAX_HELP_H
