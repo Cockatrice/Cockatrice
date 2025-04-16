1540d5aa0a2caf75586bbb36417d4ad5f154f789
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-04-16 12:46:21 +0200

    Implement deck analytics widgets.

--- cockatrice/src/client/tabs/visual_deck_editor/tab_deck_editor_visual_tab_widget.h
+++ cockatrice/src/client/tabs/visual_deck_editor/tab_deck_editor_visual_tab_widget.h
@@ -1,8 +1,10 @@
 #ifndef TAB_DECK_EDITOR_VISUAL_TAB_WIDGET_H
 #define TAB_DECK_EDITOR_VISUAL_TAB_WIDGET_H
 
+#include "../../ui/widgets/deck_analytics/deck_analytics_widget.h"
 #include "../../ui/widgets/printing_selector/printing_selector.h"
 #include "../../ui/widgets/visual_database_display/visual_database_display_widget.h"
+#include "../../ui/widgets/visual_deck_editor/visual_deck_editor_sample_hand_widget.h"
 #include "../../ui/widgets/visual_deck_editor/visual_deck_editor_widget.h"
 #include "../abstract_tab_deck_editor.h"
 
@@ -29,8 +31,10 @@ public:
     int getTabCount() const;
 
     VisualDeckEditorWidget *visualDeckView;
+    DeckAnalyticsWidget *deckAnalytics;
     VisualDatabaseDisplayWidget *visualDatabaseDisplay;
     PrintingSelector *printingSelector;
+    VisualDeckEditorSampleHandWidget *sampleHandWidget;
 
 public slots:
     void onCardChanged(CardInfoPtr activeCard);
