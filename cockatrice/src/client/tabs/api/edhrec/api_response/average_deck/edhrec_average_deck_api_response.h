0a811ffcfb9918bafd1743c521f2f986f0c6eef1
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-04-17 00:21:53 +0200

    Lint.

--- cockatrice/src/client/tabs/api/edhrec/api_response/average_deck/edhrec_average_deck_api_response.h
+++ cockatrice/src/client/tabs/api/edhrec/api_response/average_deck/edhrec_average_deck_api_response.h
@@ -1,10 +1,10 @@
 #ifndef EDHREC_AVERAGE_DECK_API_RESPONSE_H
 #define EDHREC_AVERAGE_DECK_API_RESPONSE_H
 
-#include "edhrec_deck_api_response.h"
 #include "../commander/edhrec_commander_api_response_archidekt_links.h"
 #include "../commander/edhrec_commander_api_response_average_deck_statistics.h"
 #include "../commander/edhrec_commander_api_response_card_container.h"
+#include "edhrec_deck_api_response.h"
 
 #include <QDebug>
 #include <QJsonObject>
