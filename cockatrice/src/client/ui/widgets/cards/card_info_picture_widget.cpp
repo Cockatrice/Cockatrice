74e9fcb7a1939d59649e4303f97cdf26d09ccb02
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-04-16 16:10:37 +0200

    Only emit cardClicked if it wasn't a right click.

--- cockatrice/src/client/ui/widgets/cards/card_info_picture_widget.cpp
+++ cockatrice/src/client/ui/widgets/cards/card_info_picture_widget.cpp
@@ -282,6 +282,8 @@ void CardInfoPictureWidget::mousePressEvent(QMouseEvent *event)
     QWidget::mousePressEvent(event);
     if (event->button() == Qt::RightButton) {
         createRightClickMenu()->popup(QCursor::pos());
+    } else {
+        emit cardClicked();
     }
 }
 
