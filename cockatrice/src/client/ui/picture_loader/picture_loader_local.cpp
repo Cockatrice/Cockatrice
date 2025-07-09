01c7c41d4667bbc79a0af010a6e72c7a8f7a7103
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2025-07-09 21:48:23 +0200

    Even more naming schemes.
    Took 6 minutes

--- cockatrice/src/client/ui/picture_loader/picture_loader_local.cpp
+++ cockatrice/src/client/ui/picture_loader/picture_loader_local.cpp
@@ -83,17 +83,25 @@ QImage PictureLoaderLocal::tryLoadCardImageFromDisk(const QString &setName,
     QImageReader imgReader;
     imgReader.setDecideFormatFromContent(true);
 
-    // Order is cardName_providerId, cardName_setName_collectorNumber, setName-collectorNumber-cardName and then just
-    // generically cardName, if the user has decided to override every printing. Most-to-least specific.
+    // Most-to-least specific, these will fall through in order.
 
     QStringList nameVariants;
+    // cardName_providerId
     if (!providerId.isEmpty()) {
+        nameVariants << QString("%1-%2").arg(correctedCardName, providerId);
         nameVariants << QString("%1_%2").arg(correctedCardName, providerId);
     }
+    // cardName_setName_collectorNumber & setName-collectorNumber-cardName
     if (!setName.isEmpty() && !collectorNumber.isEmpty()) {
         nameVariants << QString("%1_%2_%3").arg(correctedCardName, setName, collectorNumber)
                      << QString("%1-%2-%3").arg(setName, collectorNumber, correctedCardName);
     }
+    // cardName_setName
+    if (!setName.isEmpty()) {
+        nameVariants << QString("%1_%2").arg(correctedCardName, setName)
+                     << QString("%1-%2").arg(setName, correctedCardName);
+    }
+    // cardName
     nameVariants << correctedCardName;
 
     for (const QString &nameVariant : nameVariants) {
