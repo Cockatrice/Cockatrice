5381562a5ea7d222cbef9945c2e6ba2d7252bdb3
BruebachL <44814898+BruebachL@users.noreply.github.com>
2025-09-28 12:39:40 +0200

    [TabGame/GameEventHandler] Re-emit spectator addition signals in eventGameStateChanged (#6187)
    * [TabGame/GameEventHandler] Re-emit spectator addition signals in eventGameStateChanged.
    
    Took 36 minutes
    
    * Check spectators as a whole.
    
    Took 2 minutes
    
    * Lint.
    
    Took 42 seconds
    
    ---------
    
    Co-authored-by: Lukas Brübach <Bruebach.Lukas@bdosecurity.de>

--- cockatrice/src/game/game_event_handler.cpp
+++ cockatrice/src/game/game_event_handler.cpp
@@ -251,7 +251,11 @@ void GameEventHandler::eventGameStateChanged(const Event_GameStateChanged &event
         QString playerName = "@" + QString::fromStdString(prop.user_info().name());
         emit addPlayerToAutoCompleteList(playerName);
         if (prop.spectator()) {
-            game->getPlayerManager()->addSpectator(playerId, prop);
+            if (!game->getPlayerManager()->getSpectators().contains(playerId)) {
+                game->getPlayerManager()->addSpectator(playerId, prop);
+                emit spectatorJoined(prop);
+                emit logJoinSpectator(playerName);
+            }
         } else {
             Player *player = game->getPlayerManager()->getPlayers().value(playerId, 0);
             if (!player) {
