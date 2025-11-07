98559005a2412a686ea3631b12235d0e24b9a1c3
ZeldaZach <zahalpern+github@gmail.com>
2025-11-07 13:22:48 -0500

    Strip dashes in Yahoo addresses

--- servatrice/src/email_parser.cpp
+++ servatrice/src/email_parser.cpp
@@ -25,16 +25,21 @@ QPair<QString, QString> EmailParser::parseEmailAddress(const QString &dirtyEmail
 
     // Trim out dots and pluses from Google/Gmail domains
     if (capturedEmailAddressDomain.toLower() == "gmail.com") {
-        // Remove all content after first plus sign (as unnecessary with gmail)
+        // Remove all content after the first plus sign (as unnecessary with gmail)
         // https://gmail.googleblog.com/2008/03/2-hidden-ways-to-get-more-from-your.html
-        const auto firstPlusSign = capturedEmailUser.indexOf("+");
-        if (firstPlusSign != -1) {
+        if (const auto firstPlusSign = capturedEmailUser.indexOf("+"); firstPlusSign != -1) {
             capturedEmailUser = capturedEmailUser.left(firstPlusSign);
         }
 
         // Remove all periods (as unnecessary with gmail)
         // https://gmail.googleblog.com/2008/03/2-hidden-ways-to-get-more-from-your.html
         capturedEmailUser.replace(".", "");
+    }
+    // Trim out minuses from Yahoo domains
+    else if (capturedEmailAddressDomain.toLower() == "yahoo.com") {
+        if (const auto firstMinusSign = capturedEmailUser.indexOf("-"); firstMinusSign != -1) {
+            capturedEmailUser = capturedEmailUser.left(firstMinusSign);
+        }
     }
 
     return {capturedEmailUser, capturedEmailAddressDomain};
