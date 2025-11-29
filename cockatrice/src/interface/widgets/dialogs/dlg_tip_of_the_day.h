0fd3b524b24e11f678b488611f28d66dfd349ee2
Brübach, Lukas <lukas.bruebach@student.fhws.de>
2025-11-29 15:19:55 +0100

    [Cleanup] Redo fwd declarations.

--- cockatrice/src/interface/widgets/dialogs/dlg_tip_of_the_day.h
+++ cockatrice/src/interface/widgets/dialogs/dlg_tip_of_the_day.h
@@ -7,17 +7,18 @@
 #ifndef DLG_TIPOFDAY_H
 #define DLG_TIPOFDAY_H
 
-#include <QCheckBox>
 #include <QComboBox>
 #include <QDialog>
 #include <QDialogButtonBox>
-#include <QLabel>
 #include <QLineEdit>
 #include <QLoggingCategory>
 #include <QVBoxLayout>
 
 inline Q_LOGGING_CATEGORY(DlgTipOfTheDayLog, "dlg_tip_of_the_day");
 
+class QLabel;
+class QPushButton;
+class QCheckBox;
 class TipsOfTheDay;
 
 class DlgTipOfTheDay : public QDialog
