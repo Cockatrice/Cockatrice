337360c628d7e578ceac1f5b73128c7f0d37e84c
tooomm <tooomm@users.noreply.github.com>
2026-03-08 18:13:49 +0100

    remove unused zip command

--- .ci/name_build.sh
+++ .ci/name_build.sh
@@ -2,7 +2,6 @@
 # used by the ci to rename build artifacts
 # renames the file to [original name][SUFFIX].[original extension]
 # where SUFFIX is either available in the environment or as the first arg
-# if MAKE_ZIP is set instead a zip is made
 # expected to be run in the build directory unless BUILD_DIR is set
 # adds output to GITHUB_OUTPUT
 builddir="${BUILD_DIR:=.}"
@@ -22,8 +21,8 @@ set -e
 
 # find file
 found="$(find "$builddir" -maxdepth 1 -type f -name "$findrx" -print -quit)"
-path="${found%/*}" # remove all after last /
-file="${found##*/}" # remove all before last /
+path="${found%/*}"    # remove all including first "/" from right side
+file="${found##*/}"    # remove all including last "/" from left side
 if [[ ! $file ]]; then
   echo "::error file=$0::could not find package"
   exit 1
@@ -35,18 +34,12 @@ if ! cd "$path"; then
 fi
 
 # set filename
-name="${file%.*}" # remove file extension
+name="${file%.*}"    # remove all including first "." from right side
 new_name="$name$SUFFIX"
-if [[ $MAKE_ZIP ]]; then
-  filename="${new_name}.zip"
-  echo "creating zip '$filename' from '$file'"
-  zip "$filename" "$file"
-else
-  extension="${file##*.}" # remove all before last .
-  filename="$new_name.$extension"
-  echo "renaming '$file' to '$filename'"
-  mv "$file" "$filename"
-fi
+extension="${file##*.}"    # remove all including last "." from left side
+filename="$new_name.$extension"
+echo "renaming '$file' to '$filename'"
+mv "$file" "$filename"
 
 cd "$oldpwd"
 relative_path="$path/$filename"
