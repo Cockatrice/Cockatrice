dcb9ed0044708605a9e924c8c435ee4390681d88
tooomm <tooomm@users.noreply.github.com>
2026-06-21 18:06:48 +0200

    boolean

--- .ci/prep_release.sh
+++ .ci/prep_release.sh
@@ -23,18 +23,18 @@ fi
 
 # create title
 if [[ $TAG =~ $beta_regex ]]; then
-  echo "is_beta=yes" >>"$GITHUB_OUTPUT"
+  echo "is_beta=true" >>"$GITHUB_OUTPUT"
   title="$TAG"
   echo "creating beta release '$title'"
 elif [[ ! $(cat CMakeLists.txt) =~ $name_regex ]]; then
   echo "::error file=$0::could not find releasename in CMakeLists.txt"
   exit 1
 else
-  echo "is_beta=no" >>"$GITHUB_OUTPUT"
+  echo "is_beta=false" >>"$GITHUB_OUTPUT"
   name="${BASH_REMATCH[1]}"
   version="${TAG##*-}"
   title="Cockatrice $version: $name"
-  no_beta=1
+  no_beta=true
   echo "friendly_name=$name" >>"$GITHUB_OUTPUT"
   echo "creating full release '$title'"
 fi
