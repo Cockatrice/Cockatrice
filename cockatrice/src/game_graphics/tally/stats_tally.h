ca1c06368711cbdcec050dc445de89ef920d624c
RickyRister <42636155+RickyRister@users.noreply.github.com>
2026-08-02 19:20:47 -0700

    [Game] Implement total power tally (#7057)
    * [Game] Implement total power tally
    
    * PR comments

---
+++ cockatrice/src/game_graphics/tally/stats_tally.h
@@ -0,0 +1,21 @@
+#ifndef COCKATRICE_STATS_TALLY_H
+#define COCKATRICE_STATS_TALLY_H
+#include "tally.h"
+
+/**
+ * @brief Extracts and tallies stats from selected cards.
+ */
+namespace StatsTally
+{
+
+/**
+ * @brief Sums the power of all selected cards
+ *
+ * @param cards The list of selected card items to analyze.
+ * @return A single row containing the total, or an empty list if none of the cards have pt
+ */
+QList<TallyRow> computeTotalPower(const QList<CardItem *> &cards);
+
+} // namespace StatsTally
+
+#endif // COCKATRICE_STATS_TALLY_H
