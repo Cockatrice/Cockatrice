4b6a6d71742304fb074fe413d4039c3cd598fa4a
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2026-08-13 14:22:57 +0200

    [VDS] Drive folder and preview widgets from the model (MVC views)
    Took 16 minutes
    
    Took 8 minutes
    
    Took 3 minutes
    
    
    Took 11 minutes

--- cockatrice/src/interface/widgets/visual_deck_storage/deck_preview/deck_preview_color_identity_filter_widget.h
+++ cockatrice/src/interface/widgets/visual_deck_storage/deck_preview/deck_preview_color_identity_filter_widget.h
@@ -2,18 +2,17 @@
  * @file deck_preview_color_identity_filter_widget.h
  * @ingroup VisualDeckPreviewWidgets
  */
-//! \todo Document this file.
 
 #ifndef DECK_PREVIEW_COLOR_IDENTITY_FILTER_WIDGET_H
 #define DECK_PREVIEW_COLOR_IDENTITY_FILTER_WIDGET_H
 
-#include "../visual_deck_storage_widget.h"
+#include "../visual_deck_storage_sort_filter_proxy_model.h"
 
 #include <QHBoxLayout>
+#include <QMap>
 #include <QPushButton>
 #include <QWidget>
 
-class DeckPreviewWidget;
 class VisualDeckStorageWidget;
 
 class DeckPreviewColorIdentityFilterWidget : public QWidget
@@ -21,34 +20,21 @@ class DeckPreviewColorIdentityFilterWidget : public QWidget
     Q_OBJECT
 
 public:
-    /**
-     * How the active colors are matched against a deck's color identity.
-     */
-    enum FilterMode
-    {
-        ExactMatch, ///< The color identity consists of exactly the active colors.
-        Includes,   ///< The color identity contains all of the active colors.
-        Excludes    ///< The color identity contains none of the active colors.
-    };
-    Q_ENUM(FilterMode)
-
     explicit DeckPreviewColorIdentityFilterWidget(VisualDeckStorageWidget *parent);
     void retranslateUi();
-    void filterWidgets(QList<DeckPreviewWidget *> widgets);
-
-signals:
-    void filterModeChanged(FilterMode mode);
-    void activeColorsChanged();
 
 private slots:
     void handleColorToggled(QChar color, bool active);
     void updateFilterMode();
 
 private:
+    void applyFilter();
+
     QHBoxLayout *layout;
     QPushButton *toggleButton;
     QMap<QChar, bool> activeColors;
-    FilterMode filterMode = Includes; // Default to "includes" mode
+    VisualDeckStorageSortFilterProxyModel::FilterMode filterMode = VisualDeckStorageSortFilterProxyModel::Includes;
+    VisualDeckStorageWidget *visualDeckStorageWidget; ///< The VDS owning the proxy model.
 };
 
 #endif // DECK_PREVIEW_COLOR_IDENTITY_FILTER_WIDGET_H
