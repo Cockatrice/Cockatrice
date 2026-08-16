cfb958038ca1c8b512a7c21f8fc548fe375be5d6
Lukas Brübach <Bruebach.Lukas@bdosecurity.de>
2026-08-16 17:24:23 +0200

    [Server] Consolidate Server_Game construction parameters into a GameConfig struct

---
+++ libcockatrice_network/libcockatrice/network/server/remote/game/game_config.h
@@ -0,0 +1,26 @@
+#ifndef GAME_CONFIG_H
+#define GAME_CONFIG_H
+
+#include <QList>
+#include <QString>
+#include <libcockatrice/protocol/pb/serverinfo_user.pb.h>
+
+struct GameConfig
+{
+    ServerInfo_User creatorInfo;
+    int gameId = -1;
+    QString description;
+    QString password;
+    int maxPlayers = 2;
+    QList<int> gameTypes;
+    bool onlyBuddies = false;
+    bool onlyRegistered = false;
+    bool spectatorsAllowed = false;
+    bool spectatorsNeedPassword = false;
+    bool spectatorsCanTalk = true;
+    bool spectatorsSeeEverything = true;
+    int startingLifeTotal = 20;
+    bool shareDecklistsOnLoad = true;
+};
+
+#endif
